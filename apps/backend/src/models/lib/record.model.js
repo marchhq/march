68917e9a8e4e8dd7cfbd1040f0144126f294ca2f
sajdkabir <sajda.kbir@gmail.com>
2024-07-28 16:57:25 +0530

    feat: added rocord model

---
+++ apps/backend/src/models/lib/record.model.js
@@ -0,0 +1,52 @@
+import { Schema } from "mongoose";
+import { v4 as uuid } from "uuid";
+import { db } from "../../loaders/db.loader.js";
+
+const statusChoices = ["inbox", "todo", "in progress", "done"];
+
+const RecordSchema = new Schema({
+    uuid: {
+        type: String,
+        default: () => uuid()
+    },
+    title: {
+        type: String
+    },
+    content: {
+        type: String,
+        default: ''
+    },
+    status: {
+        type: String,
+        enum: statusChoices,
+        default: "inbox"
+    },
+    dueDate: {
+        type: Date,
+        default: null
+    },
+    page: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Page'
+    }],
+    user: {
+        type: Schema.Types.String
+        // ref: 'User'
+    },
+    isArchived: {
+        type: Boolean,
+        default: false
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const Record = db.model('Record', RecordSchema, 'records')
+
+export {
+    Record
+}
