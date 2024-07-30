784ac007df0bcf8d74b82b31d5b14240e1c02ab8
sajdkabir <sajda.kbir@gmail.com>
2024-07-30 19:38:34 +0530

    feat: added setDate item

--- apps/backend/src/services/lib/integration.service.js
+++ apps/backend/src/services/lib/integration.service.js
@@ -9,6 +9,19 @@ const getIntegration = async (user) => {
     return issues;
 };
 
+const moveItemtoDate = async (date, id) => {
+    const formattedDate = new Date(date); // Ensure date is a Date object
+
+    const item = await Integration.findByIdAndUpdate(
+        id,
+        { $set: { date: formattedDate } }, // Correctly specify the field to update
+        { new: true } // Ensure timestamps are updated automatically by Mongoose
+    );
+
+    return item;
+};
+
 export {
-    getIntegration
+    getIntegration,
+    moveItemtoDate
 }
