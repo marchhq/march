cbce99f29f82843da438688db373c49979d846ed
Xron Trix <xrontrix@gmail.com>
2024-08-06 15:00:33 +0530

    fix(design): fixed meeting page design

--- apps/frontend/app/app/page/meeting/page.tsx
+++ apps/frontend/app/app/page/meeting/page.tsx
@@ -2,16 +2,16 @@
 
 import React from "react"
 
-import { Calendar } from "@phosphor-icons/react"
-
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/atoms/Resizable"
+import EachMeet from "@/components/EachMeet"
+import PageSection from "@/components/PageSection"
 
 const MeetingPage: React.FC = () => {
-  const recents = [
+  const meets = [
     {
       title: "Interview Meeting",
       date: "4th August",
@@ -26,9 +26,6 @@ const MeetingPage: React.FC = () => {
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia iste molestiae esse!",
     },
-  ]
-
-  const upcomings = [
     {
       title: "Interview Meeting",
       date: "6th August",
@@ -52,52 +49,45 @@ const MeetingPage: React.FC = () => {
     },
   ]
 
+  const [activeMeet, setActiveMeet] = React.useState<number>(0);
+
+  const handleMeetClick = (index: number): void => {
+    setActiveMeet(index)
+  }
+
   return (
     <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={75} minSize={40}>
+      <ResizablePanel defaultSize={70} minSize={40}>
         <div className="h-full overflow-auto rounded-xl border border-white/10 bg-white/10 px-6 py-5 shadow-lg backdrop-blur-lg">
-          <div className="px-3 font-semibold text-zinc-300">Meetings</div>
-          <div className="mt-12 px-3">
-            <div className="flex flex-col gap-y-8">
-              {recents.map((meet) => (
-                <div
-                  key={meet.time}
-                  className="flex max-w-screen-sm justify-between gap-x-6 rounded-lg border border-white/5 p-3 text-sm backdrop-blur-lg"
-                >
-                  <div>
-                    <div className="flex items-center gap-x-2 text-zinc-400">
-                      <Calendar size={24} weight="duotone" />
-                      <p className="text-zinc-300">{meet.title}</p>
-                    </div>
-                    <p className="mt-2 text-xs text-zinc-400">
-                      {meet.description}
-                    </p>
-                  </div>
-                  <div className="min-w-24">
-                    <p className="text-zinc-300">{meet.date}</p>
-                    <span className="text-xs text-zinc-400">{meet.time}</span>
-                  </div>
-                </div>
-              ))}
+          <div className="mt-4 px-3">
+            <div>
+              <div className="flex items-center gap-x-2 text-sm text-zinc-300">
+                <div className="size-4 rounded-full border border-white/30 bg-white/10" />
+                <span>{meets[activeMeet].date}</span>
+                <div className="size-1 rounded-full bg-zinc-300" />
+                <span>{meets[activeMeet].time}</span>
+              </div>
+              <div className="my-5 text-2xl font-semibold text-zinc-200">
+                {meets[activeMeet].title}
+              </div>
+              <PageSection />
             </div>
           </div>
         </div>
       </ResizablePanel>
       <ResizableHandle />
-      <ResizablePanel defaultSize={25} minSize={25}>
-        <div className="h-full overflow-auto rounded-xl border border-white/10 bg-white/10 p-5 shadow-lg backdrop-blur-lg">
+      <ResizablePanel defaultSize={30} minSize={30}>
+        <div className="h-full overflow-auto rounded-xl border border-white/10 bg-white/10 px-2 py-5 shadow-lg backdrop-blur-lg">
           <div className="px-3 font-semibold text-zinc-300">Upcoming</div>
-          <div className="mt-12 flex flex-col gap-y-6 px-3">
-            {upcomings.map((meet) => (
-              <div key={meet.time}>
-                <div className="flex items-center justify-start gap-x-3 text-sm text-zinc-300">
-                  <Calendar size={16} weight="duotone" />
-                  <p>{meet.title}</p>
-                </div>
-                <p className="ml-8 mt-1 text-xs text-zinc-500">
-                  {meet.date} · {meet.time}
-                </p>
-              </div>
+          <div className="mt-12 flex flex-col gap-y-2 px-3">
+            {meets.map((meet, index) => (
+              <EachMeet
+                key={meet.title}
+                activeMeet={activeMeet}
+                setActiveMeet={handleMeetClick}
+                meet={meets[index]}
+                index={index}
+              />
             ))}
           </div>
         </div>
