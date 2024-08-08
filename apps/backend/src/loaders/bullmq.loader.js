7ef79d35370bc647aace317811304fc71e0ad202
sajdkabir <sajda.kbir@gmail.com>
2024-08-08 13:13:39 +0530

    feat: created calendarQueue

--- apps/backend/src/loaders/bullmq.loader.js
+++ apps/backend/src/loaders/bullmq.loader.js
@@ -5,8 +5,13 @@ const linearQueue = new Queue('linearQueue', {
     connection: redisConnection
 });
 
+const calendarQueue = new Queue('calendarQueue', {
+    connection: redisConnection
+});
+
 console.log('Queues setup completed.');
 
 export {
-    linearQueue
+    linearQueue,
+    calendarQueue
 };
