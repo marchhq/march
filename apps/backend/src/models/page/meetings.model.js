baa9f5c51707a8337ad7aca30dd38cb97b28ce6f
sajdkabir <sajda.kbir@gmail.com>
2024-08-08 12:12:58 +0530

    feat: added meeting model

---
+++ apps/backend/src/models/page/meetings.model.js
@@ -0,0 +1,52 @@
+import { Schema } from "mongoose";
+import { v4 as uuid } from "uuid";
+import { db } from "../../loaders/db.loader.js";
+
+const meetingSchema = new Schema({
+    uuid: {
+        type: String,
+        default: () => uuid()
+    },
+    title: {
+        type: String
+    },
+    content: {
+        type: String,
+        default: ''
+    },
+    metadata: {
+        type: Schema.Types.Mixed
+    },
+    id: {
+        type: String
+    },
+    createdAt: {
+        type: Date
+    },
+    updatedAt: {
+        type: Date
+    },
+    pages: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Page'
+    }],
+    user: {
+        type: Schema.Types.String
+    },
+    isArchived: {
+        type: Boolean,
+        default: false
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const Meeting = db.model('Meeting', meetingSchema, 'meetings')
+
+export {
+    Meeting
+}
