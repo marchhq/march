638cdb09d4355c706fd07c7cece83ebdb2f3b207
sajdkabir <sajda.kbir@gmail.com>
2024-08-12 11:02:29 +0530

    fix: item model

--- apps/backend/src/services/integration/linear.service.js
+++ apps/backend/src/services/integration/linear.service.js
@@ -24,7 +24,7 @@ const getAccessToken = async (code, user) => {
             privateMetadata: {
                 integration: {
                     linear: {
-                        accessToken: accessToken
+                        accessToken
                     }
                 }
             }
