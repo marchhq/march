6d49e7f893369360aa846fa86b2843434d813843
sajdkabir <sajda.kbir@gmail.com>
2024-08-12 09:36:07 +0530

    refactor: gcal webhook

--- apps/backend/src/controllers/integration/calendar.controller.js
+++ apps/backend/src/controllers/integration/calendar.controller.js
@@ -1,8 +1,8 @@
-import { getGoogleCalendarOAuthAuthorizationUrl, getGoogleCalendarAccessToken, getGoogleCalendarEvents, addGoogleCalendarEvent, updateGoogleCalendarEvent, deleteGoogleCalendarEvent, getGoogleCalendarMeetings, getGoogleCalendarupComingMeetings, checkAccessTokenValidity, refreshGoogleCalendarAccessToken, setUpCalendarWatch } from "../..//services/integration/calendar.service.js";
+import { getGoogleCalendarOAuthAuthorizationUrl, getGoogleCalendarAccessToken, getGoogleCalendarEvents, addGoogleCalendarEvent, updateGoogleCalendarEvent, deleteGoogleCalendarEvent, getGoogleCalendarMeetings, getGoogleCalendarupComingMeetings, checkAccessTokenValidity, refreshGoogleCalendarAccessToken, setUpCalendarWatch, handleCalendarWebhookService } from "../..//services/integration/calendar.service.js";
 import { calendarQueue } from "../../loaders/bullmq.loader.js";
 import { clerk } from "../../middlewares/clerk.middleware.js";
 import { environment } from "../../loaders/environment.loader.js";
-import { listener } from "../../../index.js";
+// import { listener } from "../../../index.js";
 
 const redirectGoogleCalendarOAuthLoginController = async (req, res, next) => {
     try {
@@ -20,10 +20,11 @@ const getGoogleCalendarAccessTokenController = async (req, res, next) => {
     const user = req.auth.userId;
     try {
         const tokenInfo = await getGoogleCalendarAccessToken(code, user);
-        const url = `${listener.url()}/calendar/webhook/?user=${user}`
-        const watchResponse = await setUpCalendarWatch(tokenInfo.access_token, 'primary', url, user)
+        // const url = `${listener.url()}/calendar/webhook/?user=${user}`
+        const url = `${environment.CALENDAR_WEBHOOK_URL}/calendar/webhook/?user=${user}`;
+        console.log("usr: ", url);
+        await setUpCalendarWatch(tokenInfo.access_token, 'primary', url, user)
         // const watchResponse = await setUpCalendarWatch(tokenInfo.access_token, 'primary', environment.CALENDAR_WEBHOOK_URL)
-        console.log("watchResponse: ", watchResponse);
         await calendarQueue.add('calendarQueue', {
             accessToken: tokenInfo.access_token,
             refreshToken: tokenInfo.refresh_token,
@@ -129,6 +130,39 @@ const deleteGoogleCalendarEventController = async (req, res, next) => {
     }
 };
 
+const handleCalendarWebhook = async (req, res, next) => {
+    const channelToken = req.headers['x-goog-channel-token'];
+    const resourceState = req.headers['x-goog-resource-state'];
+    const userId = req.query.user;
+    if (channelToken !== environment.CALENDAR_WEBHOOK_SECRET) {
+        return res.status(403).send('Invalid webhook token');
+    }
+
+    if (resourceState === 'sync') {
+        return res.status(200).send();
+    }
+
+    const user = await clerk.users.getUser(userId);
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+
+    try {
+        let accessToken = user.privateMetadata.integration.googleCalendar.accessToken;
+        const refreshToken = user.privateMetadata.integration.googleCalendar.refreshToken;
+
+        const isValid = await checkAccessTokenValidity(accessToken);
+
+        if (!isValid) {
+            accessToken = await refreshGoogleCalendarAccessToken(user);
+        }
+        await handleCalendarWebhookService(accessToken, refreshToken, userId)
+        res.status(200).send('Webhook event processed');
+    } catch (err) {
+        next(err);
+    }
+}
+
 export {
     redirectGoogleCalendarOAuthLoginController,
     getGoogleCalendarAccessTokenController,
@@ -137,5 +171,6 @@ export {
     updateGoogleCalendarEventController,
     deleteGoogleCalendarEventController,
     getGoogleCalendarMeetingsController,
-    getGoogleCalendarupComingMeetingsController
+    getGoogleCalendarupComingMeetingsController,
+    handleCalendarWebhook
 }
