60d9200fb8d97f0397b0f16a00abc4086b9f449f
Xron Trix <xrontrix@gmail.com>
2024-08-14 02:39:31 +0530

    feat: added icon svg

---
+++ apps/frontend/components/icons/Line.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Line = (): JSX.Element => {
+  return (
+    <svg
+      width="21"
+      height="1"
+      viewBox="0 0 21 1"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1 0.5H20" stroke="#676767" stroke-linecap="round" />
+    </svg>
+  )
+}
+
+export default Line
