6fac0611375f3a75e32b7186f168c364de117107
sajdkabir <sajda.kbir@gmail.com>
2024-08-14 10:51:52 +0530

    refactor: create label

--- apps/backend/src/controllers/integration/email.controller.js
+++ apps/backend/src/controllers/integration/email.controller.js
@@ -1,5 +1,5 @@
 import { OauthClient } from "../../loaders/google.loader.js";
-import { getGmailAccessToken } from "../../services/integration/email.service.js"
+import { getGmailAccessToken, createLabel } from "../../services/integration/email.service.js"
 import { google } from "googleapis";
 import { clerk } from "../../middlewares/clerk.middleware.js";
 import { environment } from "../../loaders/environment.loader.js";
@@ -129,30 +129,6 @@ const getGmailAccessTokenController = async (req, res, next) => {
     }
 };
 
-const createLabel = async (OauthClient, labelName) => {
-    try {
-        const gmail = google.gmail({ version: 'v1', auth: OauthClient });
-
-        const existingLabels = await gmail.users.labels.list({ userId: 'me' });
-        const label = existingLabels.data.labels.find(label => label.name === labelName);
-        if (label) {
-            return label.id;
-        }
-
-        const newLabel = await gmail.users.labels.create({
-            userId: 'me',
-            requestBody: {
-                labelListVisibility: 'labelShow',
-                messageListVisibility: 'show',
-                name: labelName
-            }
-        });
-
-        return newLabel.data.id;
-    } catch (error) {
-        console.error(`Error creating/checking label ${labelName}:`, error);
-    }
-};
 
 const createGmailLabelsController = async (req, res) => {
     const user = req.auth.userId;
