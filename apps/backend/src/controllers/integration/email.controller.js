2b578f888f02039c9b65808f796fc82eed7dd30d
sajdkabir <sajda.kbir@gmail.com>
2024-08-14 10:49:50 +0530

    feat: creating a lable for march

--- apps/backend/src/controllers/integration/email.controller.js
+++ apps/backend/src/controllers/integration/email.controller.js
@@ -134,23 +134,21 @@ const createLabel = async (OauthClient, labelName) => {
         const gmail = google.gmail({ version: 'v1', auth: OauthClient });
 
         const existingLabels = await gmail.users.labels.list({ userId: 'me' });
-        const labelExists = existingLabels.data.labels.some(label => label.name === labelName);
-        console.log("labelExists: ", existingLabels.data.labels);
-        let label;
-        if (!labelExists) {
-            label = await gmail.users.labels.create({
-                userId: 'me',
-                requestBody: {
-                    labelListVisibility: 'labelShow',
-                    messageListVisibility: 'show',
-                    name: labelName
-                }
-            });
-        } else {
-            console.log(`Label '${labelName}' already exists.`);
+        const label = existingLabels.data.labels.find(label => label.name === labelName);
+        if (label) {
+            return label.id;
         }
-        console.log("label: ", label.data);
-        return label.data.id;
+
+        const newLabel = await gmail.users.labels.create({
+            userId: 'me',
+            requestBody: {
+                labelListVisibility: 'labelShow',
+                messageListVisibility: 'show',
+                name: labelName
+            }
+        });
+
+        return newLabel.data.id;
     } catch (error) {
         console.error(`Error creating/checking label ${labelName}:`, error);
     }
