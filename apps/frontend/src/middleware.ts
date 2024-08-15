7fe20bee8c63b495a788254215fc87513c46f7f2
Xron Trix <xrontrix@gmail.com>
2024-08-15 21:00:13 +0530

    feat: completed jwt authentication

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -24,5 +24,6 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|auth|_next/static|_next/image|favicon.ico).*)"],
 }
+
