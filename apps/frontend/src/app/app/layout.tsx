15c36e00ffc1ea019649d631ce281ec27eacab5f
Xron Trix <xrontrix@gmail.com>
2024-08-16 23:27:37 +0530

    refactor: moved provider

--- apps/frontend/src/app/app/layout.tsx
+++ apps/frontend/src/app/app/layout.tsx
@@ -1,17 +1,22 @@
 import * as React from "react"
 
+import QueryProvider from "@/src/components/QueryProvider"
 import Sidebar from "@/src/components/Sidebar"
-
+import { AuthProvider } from "@/src/contexts/AuthContext"
 interface Props {
   children: React.ReactNode
 }
 
 const AppLayout: React.FC<Props> = ({ children }) => {
   return (
-    <main className="flex h-screen gap-1 bg-white p-1 dark:bg-black">
-      <Sidebar />
-      <section className="flex-1">{children}</section>
-    </main>
+    <AuthProvider> 
+      <QueryProvider>
+        <main className="flex h-screen gap-1 bg-white p-1 dark:bg-black">
+          <Sidebar />
+          <section className="flex-1">{children}</section>
+        </main>
+      </QueryProvider>
+    </AuthProvider>
   )
 }
 
