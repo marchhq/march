0ef9e4ecfc9ce5d21938d51a65f110a309dd8ccb
sajdkabir <sajda.kbir@gmail.com>
2024-08-19 12:59:38 +0530

    feat: remove magic link login

--- apps/backend/src/routers/core/auth.route.js
+++ apps/backend/src/routers/core/auth.route.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { magicLoginController, validateLoginMagicLinkController, authenticateWithGoogleController, logOutController, registerEmailUserController, emailLoginController } from "../../controllers/core/auth.controller.js";
+import { authenticateWithGoogleController, logOutController, registerEmailUserController, emailLoginController } from "../../controllers/core/auth.controller.js";
 
 const router = Router();
 
 router.route('/common/signup/').post(registerEmailUserController);
 router.route('/common/login/').post(emailLoginController);
-router.route('/magic/login/').post(magicLoginController);
-router.route('/magic/verify/').post(validateLoginMagicLinkController);
 
 router.route('/google/login/').post(authenticateWithGoogleController);
 
