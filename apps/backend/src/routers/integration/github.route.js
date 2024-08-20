b9adbda530417d7f0b7411861076e5752dee05d3
sajdkabir <sajda.kbir@gmail.com>
2024-08-20 15:16:25 +0530

    refactor: github app callback

--- apps/backend/src/routers/integration/github.route.js
+++ apps/backend/src/routers/integration/github.route.js
@@ -1,10 +1,7 @@
 import { Router } from "express";
-// import { getUserGithubIssuesAndPRsController, handleGithubCallbackController } from "../../controllers/integration/github.controller.js"
 import { handleGithubCallbackController } from "../../controllers/integration/github.controller.js";
 
 const router = Router();
-// router.route("/issues/").get(getUserGithubIssuesAndPRsController);
-// router.route("/callback/").get(handleGithubCallbackController);
 
 router.get('/callback/', handleGithubCallbackController);
 
