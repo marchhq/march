f55dedd9ac5c531e193a9de20687c2826b4787d3
sajdkabir <sajda.kbir@gmail.com>
2024-08-21 18:01:02 +0530

    refactor: redirect url for notion connect

--- apps/backend/src/services/integration/notion.service.js
+++ apps/backend/src/services/integration/notion.service.js
@@ -1,41 +1,6 @@
 import axios from 'axios';
 import { environment } from '../../loaders/environment.loader.js';
 
-// const getNotionAccessToken = async (code, user) => {
-//     // const tokenResponse = await axios.post('https://api.notion.com/v1/oauth/token', {
-//     //     grant_type: 'authorization_code',
-//     //     code: code,
-//     //     redirect_uri: environment.NOTION_REDIRECT_URL,
-//     //     client_id: environment.NOTION_CLIENT_ID,
-//     //     client_secret: environment.NOTION_CLIENT_SECRET
-//     // });
-
-//     const tokenResponse = await axios.post('https://api.notion.com/v1/oauth/token', {
-//         grant_type: 'authorization_code',
-//         code: code,
-//         redirect_uri: environment.NOTION_REDIRECT_URL,
-//         // client_id: environment.NOTION_CLIENT_ID,
-//         // client_secret: environment.NOTION_CLIENT_SECRET
-//     }, {
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded'
-//         }
-//     });
-    
-
-//     const accessToken = tokenResponse.data.access_token;
-    
-//       // Save the user's access token in the database
-//     //   const user = new User({ notionAccessToken: accessToken });
-//     //   await user.save();
-    
-//     //   res.send('Notion connected successfully! Now you can select a page.');
-
-//     console.log("f: ", tokenResponse.data);
-//     console.log("f2: ", accessToken);
-// }
-
-
 const getNotionAccessToken = async (code, user) => {
     try {
         const encoded = Buffer.from(`${environment.NOTION_CLIENT_ID}:${environment.NOTION_CLIENT_SECRET}`).toString("base64");
