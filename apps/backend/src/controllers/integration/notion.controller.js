7a39af98ecfab103a659a535347114da88243c62
sajdkabir <sajda.kbir@gmail.com>
2024-08-21 16:45:53 +0530

    refactor: redirectNotionAuthUrlController

--- apps/backend/src/controllers/integration/notion.controller.js
+++ apps/backend/src/controllers/integration/notion.controller.js
@@ -1,7 +1,7 @@
 import { environment } from "../../loaders/environment.loader.js";
 
 const redirectNotionAuthUrlController = (req, res) => {
-    const notionAuthUrl = environment.NOTION_REDIRECT_URL;
+    const notionAuthUrl = `https://api.notion.com/v1/oauth/authorize?client_id=${environment.NOTION_CLIENT_ID}&response_type=code&owner=user&redirect_uri=${environment.NOTION_REDIRECT_URL}`;
 
     console.log(notionAuthUrl);
     res.redirect(notionAuthUrl);
--- apps/backend/src/loaders/environment.loader.js
+++ apps/backend/src/loaders/environment.loader.js
@@ -32,7 +32,7 @@ export const environment = {
     REDIS_PORT: process.env.REDIS_PORT,
     REDIS_DB_USER: process.env.REDIS_DB_USER,
     REDIS_DB_PASS: process.env.REDIS_DB_PASS,
-    NOTION_CLIENT: process.env.NOTION_CLIENT,
+    NOTION_CLIENT_ID: process.env.NOTION_CLIENT_ID,
     NOTION_CLIENT_SECRET: process.env.NOTION_CLIENT_SECRET,
     NOTION_REDIRECT_URL: process.env.NOTION_REDIRECT_URL
 }
