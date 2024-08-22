7daf0da5c2fbab89d3fdac5a92f9bd91da2ee7ac
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-08-22 18:42:16 +0530

    style: profile page text

--- apps/frontend/src/app/app/profile/page.tsx
+++ apps/frontend/src/app/app/profile/page.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const ProfilePage: React.FC = () => {
   return (
     <section className="grid h-full place-content-center overflow-y-auto rounded-xl border border-white/10 bg-white/10 shadow-lg backdrop-blur-lg">
-      Profile Page
+      <p className="text-zinc-400">Profile Page</p>
     </section>
   )
 }
