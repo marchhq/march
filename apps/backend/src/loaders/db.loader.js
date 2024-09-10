8d546023ba3a088d00408f7d1cc04ff6b3ee7542
sajdkabir <sajda.kbir@gmail.com>
2024-09-10 15:01:43 +0530

    refactor: loafers.db

--- apps/backend/src/loaders/db.loader.js
+++ apps/backend/src/loaders/db.loader.js
@@ -5,7 +5,7 @@ import { environment } from "./environment.loader.js";
  * Creating a connection below. For now going with a apporach of username and password
  * Later will move to certificate based authentication
  * TODO: Add certificate based authentication
- *  */
+ **/
 
 const db = createConnection(`mongodb+srv://${environment.DB_USER}:${environment.DB_PASS}@${environment.DB_HOST}/${environment.DB_NAME}`, {
     autoIndex: false
