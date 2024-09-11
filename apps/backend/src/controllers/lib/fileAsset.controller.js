581f853c524c22df8682804623d0397c1f2b194c
sajdkabir <sajda.kbir@gmail.com>
2024-09-11 17:47:09 +0530

    refactor: upload file to aws s3

--- apps/backend/src/controllers/lib/fileAsset.controller.js
+++ apps/backend/src/controllers/lib/fileAsset.controller.js
@@ -1,25 +1,17 @@
-import { FileAsset } from '../../models/lib/asset.model.js';
+import { uploadFile } from "../../services/lib/fileAsset.controller.js";
 
 const uploadFileController = async (req, res, next) => {
     try {
         if (!req.file) {
             return res.status(400).json({ error: 'No file uploaded' });
         }
-
+        const user = req.user.id
         const file = req.file;
         const s3Url = file.location;
-
-        const newFileAsset = new FileAsset({
-            asset: s3Url,  
-            user: req.user.id,  
-            attributes: {},
-            isDeleted: false
-        });
-
-        await newFileAsset.save(); 
-
+        const fileAsset = await uploadFile(s3Url, user);
+        
         res.status(200).json({
-            newFileAsset
+            fileAsset
         });
     } catch (err) {
         console.error('Error uploading file: ', err);
