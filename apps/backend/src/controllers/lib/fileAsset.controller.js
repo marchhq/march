7ee6cf155821e04737afe9bb84bc3baf65435eac
sajdkabir <sajda.kbir@gmail.com>
2024-09-11 17:15:05 +0530

    refactor: file upload to s3

--- apps/backend/src/controllers/lib/fileAsset.controller.js
+++ apps/backend/src/controllers/lib/fileAsset.controller.js
@@ -1,29 +1,32 @@
 import { FileAsset } from '../../models/lib/asset.model.js';
-import { v4 as uuid } from 'uuid';
 
-// Controller to handle file upload
-export const uploadFileController = async (req, res) => {
+const uploadFileController = async (req, res, next) => {
     try {
         if (!req.file) {
             return res.status(400).json({ error: 'No file uploaded' });
         }
 
-        const file = req.file;  // Uploaded file metadata from multer
-        const s3Url = file.location;  // S3 URL from multer-s3
+        const file = req.file;
+        const s3Url = file.location;
 
-        // Store file metadata in MongoDB
         const newFileAsset = new FileAsset({
-            asset: s3Url,  // Store the S3 file URL
-            user: req.user.id,  // Assuming you have user data in req.user (e.g., from auth middleware)
-            attributes: {},  // You can pass additional file attributes here
+            asset: s3Url,  
+            user: req.user.id,  
+            attributes: {},
             isDeleted: false
         });
 
         await newFileAsset.save(); 
 
-        res.status(201).json({ message: 'File uploaded successfully', fileAsset: newFileAsset });
-    } catch (error) {
-        console.error('Error uploading file: ', error);
-        res.status(500).json({ error: 'Failed to upload file' });
+        res.status(200).json({
+            newFileAsset
+        });
+    } catch (err) {
+        console.error('Error uploading file: ', err);
+        next(err);
     }
 };
+
+export {
+    uploadFileController
+}
