0da9f2b538e19e677c5fd400304d1b844be80def
deewakar-k <dakshftw01@gmail.com>
2024-09-16 06:53:20 +0530

    resolved conflicts

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -16,8 +16,6 @@ export const metadata: Metadata = generateMetadata({
 
 const Home: React.FC = () => {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
     <main className="grid h-screen w-full place-content-center text-center">
       <div className="flex h-screen w-full max-w-7xl flex-col items-center justify-between py-8">
         <div className="flex size-full flex-col items-center justify-center">
@@ -33,59 +31,18 @@ const Home: React.FC = () => {
           </div>
 
           <div className="mt-10 w-full space-y-2 text-sm">
-=======
-    <main className="grid h-screen place-content-center text-center">
-      <div className="flex h-screen flex-col items-center justify-between py-8">
-        <div className="flex h-full flex-col items-center justify-center">
-          <Logo size={64} />
-          <h2 className="mt-12 text-3xl font-bold">Login to march</h2>
-
-          <div className="mt-10 text-[17px] text-gray-color font-medium">
-            <p>engineered for makers,</p>
-            <div className="mt-2 flex min-w-full items-center gap-x-2">
-              <p className="font-medium">designed</p> <Line />{" "}
-              <p className="text-black font-medium">to get things done.</p>
-            </div>
-          </div>
-
-          <div className="mt-10 text-sm space-y-2">
->>>>>>> 1cbb6a1 (added onboarding ui)
-=======
-    <main className="grid h-screen place-content-center text-center">
-      <div className="flex h-screen flex-col items-center justify-between py-8">
-        <div className="flex h-full flex-col items-center justify-center">
-          <Logo size={64} />
-          <h2 className="mt-12 text-3xl font-bold">Login to march</h2>
-
-          <div className="mt-10 text-[17px] text-gray-color font-medium">
-            <p>engineered for makers,</p>
-            <div className="mt-2 flex min-w-full items-center gap-x-2">
-              <p className="font-medium">designed</p> <Line />{" "}
-              <p className="text-black font-medium">to get things done.</p>
-            </div>
-          </div>
-
-          <div className="mt-10 text-sm space-y-2">
->>>>>>> 1cbb6a173a688ea32842d4f7155a8cfa45f8c94a
             <GoogleLogin />
             <GithubLogin />
           </div>
         </div>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
         <div className="w-full max-w-lg text-xs text-gray-color">
-=======
-        <div className="max-w-72 text-xs text-gray-color mt-6 mb-10">
->>>>>>> 1cbb6a1 (added onboarding ui)
-=======
-        <div className="max-w-72 text-xs text-gray-color mt-6 mb-10">
->>>>>>> 1cbb6a173a688ea32842d4f7155a8cfa45f8c94a
-          <p>By continuing, you agree to our </p>
-          <p>
-            <span className="text-black">terms of services</span> and our{" "}
-            <span className="text-black">privacy policy.</span>
-          </p>
+          <div className="max-w-72 text-xs text-gray-color mt-6 mb-10">
+            <p>By continuing, you agree to our </p>
+            <p>
+              <span className="text-black">terms of services</span> and our{" "}
+              <span className="text-black">privacy policy.</span>
+            </p>
+          </div>
         </div>
       </div>
     </main>
--- apps/frontend/src/components/atoms/GithubLogin.tsx
+++ apps/frontend/src/components/atoms/GithubLogin.tsx
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 "use client"
 
-import useGitHubLogin from "@/src/hooks/useGithubLogin"
-import { Github } from "@/src/lib/icons/Github"
-
-export const GithubLogin = (): JSX.Element => {
-  const handleLogin = useGitHubLogin()
-
-  return (
-    <button
-      onClick={handleLogin}
-      className="flex w-96 items-center justify-center gap-x-6 rounded-2xl border border-button-stroke bg-transparent p-3 font-semibold text-black"
-    >
-      <Github />
-      <span className="pr-1">Continue with github</span>
-=======
-=======
->>>>>>> 1cbb6a173a688ea32842d4f7155a8cfa45f8c94a
 import { Github } from "@/src/lib/icons/Github"
 
 export const GithubLogin = (): JSX.Element => {
@@ -25,10 +7,6 @@ export const GithubLogin = (): JSX.Element => {
     <button className="flex w-96 items-center justify-center gap-x-6 rounded-2xl border border-button-stroke bg-transparent p-3.5 text-black font-semibold">
       <Github />
       <span className="pr-1">Continue with Github</span>
-<<<<<<< HEAD
->>>>>>> 1cbb6a1 (added onboarding ui)
-=======
->>>>>>> 1cbb6a173a688ea32842d4f7155a8cfa45f8c94a
     </button>
   )
 }
