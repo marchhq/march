cd4a399f5073748a6a2e95d4c02a1c4f1cd8ef36
deewakar-k <dakshftw01@gmail.com>
2024-09-16 07:32:34 +0530

    added redirect for github login

--- apps/frontend/src/components/atoms/GithubLogin.tsx
+++ apps/frontend/src/components/atoms/GithubLogin.tsx
@@ -1,12 +1,18 @@
 "use client"
 
+import useGitHubLogin from "@/src/hooks/useGithubLogin"
 import { Github } from "@/src/lib/icons/Github"
 
 export const GithubLogin = (): JSX.Element => {
+  const handleLogin = useGitHubLogin()
+
   return (
-    <button className="flex w-96 items-center justify-center gap-x-6 rounded-2xl border border-button-stroke bg-transparent p-3.5 text-black font-semibold">
+    <button
+      onClick={handleLogin}
+      className="flex w-96 items-center justify-center gap-x-6 rounded-2xl border border-button-stroke bg-transparent p-3.5 text-black font-semibold"
+    >
       <Github />
-      <span className="pr-1">Continue with Github</span>
+      <span className="pr-1">Continue with github</span>
     </button>
   )
 }
