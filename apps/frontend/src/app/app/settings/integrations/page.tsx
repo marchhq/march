f2c989e6046f3d1d167ab52cfa8d303ff2be2d8c
deewakar-k <dakshftw01@gmail.com>
2024-09-16 11:20:34 +0530

    styling integration cards

--- apps/frontend/src/app/app/settings/integrations/page.tsx
+++ apps/frontend/src/app/app/settings/integrations/page.tsx
@@ -5,7 +5,12 @@ const Integrations = (): JSX.Element => {
   return (
     <div className="ml-20 mt-1">
       <Heading label="Integrations" />
-      <IntegrationCard />
+      <div className="grid grid-cols-3 gap-2">
+        <IntegrationCard />
+        <IntegrationCard />
+        <IntegrationCard />
+        <IntegrationCard />
+      </div>
     </div>
   )
 }
