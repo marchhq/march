6def999a34710ce5d99f48d438f8cc282c44c896
deewakar-k <dakshftw01@gmail.com>
2024-09-17 21:09:42 +0530

    added account page

--- apps/frontend/src/components/atoms/ui/card.tsx
+++ apps/frontend/src/components/atoms/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border border-neutral-200 bg-white text-neutral-950 shadow-sm dark:border-neutral-800 dark:bg-neutral-950 dark:text-neutral-50",
+      "rounded-lg  bg-white text-neutral-950 shadow-sm dark:border-neutral-800 dark:bg-neutral-950 dark:text-neutral-50",
       className
     )}
     {...props}
