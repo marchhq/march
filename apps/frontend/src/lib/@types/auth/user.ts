162a4508b2a706965ca4a37736b62ecddfb99c44
deewakar-k <dakshftw01@gmail.com>
2024-09-19 08:25:53 +0530

    added functional account page

--- apps/frontend/src/lib/@types/auth/user.ts
+++ apps/frontend/src/lib/@types/auth/user.ts
@@ -1,8 +1,20 @@
 /**
  * User type.
  */
+
+export interface AuthAccount {
+  email?: string
+  isVerified: boolean
+}
+
 export interface User {
   fullName: string
   avatar: string
   timezone: string
+  userName: string
+  accounts: {
+    local: AuthAccount
+    google: AuthAccount
+    github: AuthAccount
+  }
 }
