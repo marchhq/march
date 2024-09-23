bcff17e9d6a29e8375e4cc8d0b8b4b50dd50bb5d
sajdkabir <sajda.kbir@gmail.com>
2024-09-23 21:09:33 +0530

    feat: background

--- apps/extension/background.js
+++ apps/extension/background.js
@@ -1,14 +1,30 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "createIssue",
-        title: "Create Issue from Selection",
-        contexts: ["selection"]
+chrome.action.onClicked.addListener((tab) => {
+    // Send a message to the content script to get the heading and URL
+    chrome.tabs.sendMessage(tab.id, { action: 'getPageData' }, (response) => {
+        if (response) {
+            const { heading, url } = response;
+            console.log("saju: ", response);
+            // Send data to your backend to create an issue
+            createIssue(heading, url);
+        }
     });
 });
 
-chrome.contextMenus.onClicked.addListener((info) => {
-    const selectedText = info.selectionText;
-
-    // Optionally, open the popup with the selected text as the title
-    chrome.tabs.create({ url: chrome.runtime.getURL("popup/popup.html") });
-});
+function createIssue(title, url) {
+    fetch('http://localhost:8080/api/issues', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title,
+            description: url
+        })
+    }).then(response => {
+        if (response.ok) {
+            console.log('Issue created successfully!');
+        } else {
+            console.error('Failed to create issue.');
+        }
+    });
+}
