42e9d8801ba4ff59368cf0133cc0580490cb294e
deewakar-k <dakshftw01@gmail.com>
2024-09-23 17:58:37 +0530

    today ui

--- apps/frontend/src/app/(app)/today/page.tsx
+++ apps/frontend/src/app/(app)/today/page.tsx
@@ -1,9 +1,44 @@
+"use client"
 import * as React from "react"
 
-import DayView from "@/src/components/DayView"
+import { ChevronLeft, ChevronRight, Square, SquareCheck } from "lucide-react"
+
+import { ShowAgenda } from "@/src/components/atoms/ShowAgenda"
+import { TodayTextArea } from "@/src/components/TodayTextArea"
+import { TodayCal } from "@/src/lib/icons/Calendar"
 
 const TodayPage: React.FC = () => {
-  return <DayView />
+  return (
+    <main className="ml-36 text-gray-color">
+      <section className=" mt-4 flex max-w-[96%] items-center justify-end gap-4">
+        <span className="text-[11px] font-medium text-white">show agenda</span>
+        <ShowAgenda />
+      </section>
+      <section>
+        <header className="flex items-center justify-start gap-4">
+          <TodayCal />
+          <div>
+            <h1 className="text-xl font-medium text-white">Today</h1>
+            <p className="text-sm">Tue, 6 Aug 24</p>
+          </div>
+          <div className="flex items-center justify-between"></div>
+        </header>
+      </section>
+      <section className="mt-6">
+        <TodayTextArea />
+      </section>
+      <section className="space-y-8 text-[16px]">
+        <div className="flex items-center justify-start gap-2 text-white">
+          <Square />
+          <span>how to get product market fit </span>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <SquareCheck />
+          <span> SAT 23: Revert back to normal auth </span>
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default TodayPage
---
+++ apps/frontend/src/lib/icons/Navigation.tsx
@@ -0,0 +1,23 @@
+export const Left = (): JSX.Element => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M8.98867 12.794C8.54934 12.3546 8.54935 11.6423 8.98868 11.203L13.4535 6.73816C13.7753 6.41644 14.2591 6.32018 14.6795 6.49426C15.0998 6.66835 15.374 7.0785 15.374 7.53349L15.3753 16.4646C15.3754 16.9197 15.1013 17.33 14.6809 17.5041C14.2605 17.6783 13.7766 17.5821 13.4548 17.2603L8.98867 12.794Z"
+        fill="white"
+        fill-opacity="0.5"
+      />
+    </svg>
+  )
+}
+
+export const Right = (): JSX.Element => {
+  return
+}
