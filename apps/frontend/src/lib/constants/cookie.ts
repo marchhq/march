86d2b69e5473504d5373505ac35867916d06b399
deewakar-k <dakshftw01@gmail.com>
2024-09-24 18:35:31 +0530

    refactor: added unique token name for extension/integration

--- apps/frontend/src/lib/constants/cookie.ts
+++ apps/frontend/src/lib/constants/cookie.ts
@@ -3,7 +3,12 @@
  */
 export const ACCESS_TOKEN = "__ACCESS_TOKEN__"
 
+//for integration/extensions
+export const MARCH_ACCESS_TOKEN = "__MARCH_ACCESS_TOKEN"
+
 /**
  * REFRESH_TOKEN cookie name
  */
 export const REFRESH_TOKEN = "__REFRESH_TOKEN__"
+
+export const MARCH_REFRESH_TOKEN = "__MARCH_REFRESH__TOKEN"
