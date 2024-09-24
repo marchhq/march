179a0b824a72b08fc0b301be8c8fcb46b8a5ea40
sajdkabir <sajda.kbir@gmail.com>
2024-09-25 00:11:42 +0530

    feat: set multi url

--- apps/extension/background.js
+++ apps/extension/background.js
@@ -1,71 +1,6 @@
-// chrome.action.onClicked.addListener((tab) => {
-//     // Check if auth token exists in local storage
-//     chrome.storage.local.get(["authToken"], (result) => {
-//       const authToken = result.authToken;
-//       console.log("authToken: ", authToken);
-  
-//       if (!authToken) {
-//         console.log("hry");
-//         // If no token
-//         chrome.tabs.create({ url: "http://localhost:8080/login" });
-//       } else {
-//         // If token exists, proceed with creating the issue
-//         chrome.scripting.executeScript(
-//           {
-//             target: { tabId: tab.id },
-//             function: getPageInfo,
-//           },
-//           (results) => {
-//             if (results && results[0].result) {
-//               const { url, title } = results[0].result;
-//               createIssue(url, title, authToken);
-//             }
-//           }
-//         );
-//       }
-//     });
-//   });
-  
-//   // Function to get the current page's URL and title
-//   function getPageInfo() {
-//     return {
-//       url: window.location.href,
-//       title: document.title,
-//     };
-//   }
-  
-//   // Function to create the issue using the stored auth token
-//   async function createIssue(url, title, authToken) {
-//     try {
-//       const response = await fetch("http://localhot:8080/item/create", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Bearer ${authToken}`,
-//         },
-//         body: JSON.stringify({
-//           title: title,
-//           url: url,
-//           content: `Captured URL: ${url}`,
-//         }),
-//       });
-  
-//       if (response.ok) {
-//         console.log("Issue created successfully");
-//       } else {
-//         console.error("Failed to create issue:", response.statusText);
-//         // Handle invalid token (e.g., if the token has expired)
-//         if (response.status === 401) {
-//           chrome.storage.local.remove("authToken", () => {
-//             alert("Authentication expired. Please log in again.");
-//             chrome.tabs.create({ url: "https://your-app.com/login" });
-//           });
-//         }
-//       }
-//     } catch (error) {
-//       console.error("Error creating issue:", error);
-//     }
-//   }
+const frontendURL ='http://localhost:3000'||'https://app.march.cat.com';
+
+  const backendURL ='http://localhost:8080'||'https://sage.march.cat.com';
 
 
 chrome.action.onClicked.addListener((tab) => {
@@ -75,8 +10,6 @@ chrome.action.onClicked.addListener((tab) => {
   }, (results) => {
     if (results && results[0].result) {
       const { title, url } = results[0].result;
-      console.log("title: ", title);
-      console.log("url: ", url);
       createIssue(title, url);
     }
   });
@@ -87,11 +20,12 @@ function getPageDetails() {
 }
 
 function createIssue(title, url) {
-  // Fetch the token from cookies (stored after user login)
-  chrome.cookies.get({ url: 'http://localhost:3000', name: '__MARCH_ACCESS_TOKEN__' }, (cookie) => {
-    console.log("heu");
+  // chrome.cookies.get({ url: 'http://localhost:3000', name: '__MARCH_ACCESS_TOKEN__' }, (cookie) => {
+  chrome.cookies.get({ url: frontendURL, name: '__MARCH_ACCESS_TOKEN__' }, (cookie) => {
+
     if (cookie) {
-      fetch('http://localhost:8080/api/items/create/', {
+      // fetch("http://localhost:8080/api/items/create/", {
+      fetch(`${backendURL}/api/items/create/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -119,3 +53,5 @@ function createIssue(title, url) {
     }
   });
 }
+
+
