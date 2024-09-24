76b5d33b53663f805a0483e2d0b375ae6288785b
deewakar-k <dakshftw01@gmail.com>
2024-09-24 10:38:19 +0530

    added icons and duedate for items

--- apps/frontend/src/app/(app)/today/page.tsx
+++ apps/frontend/src/app/(app)/today/page.tsx
@@ -1,12 +1,34 @@
 "use client"
 import * as React from "react"
 
-import { ChevronLeft, ChevronRight, Square, SquareCheck } from "lucide-react"
-
 import { ShowAgenda } from "@/src/components/atoms/ShowAgenda"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/src/components/atoms/Tooltip"
 import { TodayTextArea } from "@/src/components/TodayTextArea"
+import { Box, CheckedBox } from "@/src/lib/icons/Box"
 import { TodayCal } from "@/src/lib/icons/Calendar"
+import { GithubToday } from "@/src/lib/icons/Github"
+import { Link } from "@/src/lib/icons/Link"
 import { LeftChevron, RightChevron } from "@/src/lib/icons/Navigation"
+import { Space } from "@/src/lib/icons/Space"
+
+const todos = [
+  {
+    icon: <Link />,
+    description: "how to get product market fit",
+    completion: false,
+  },
+  {
+    icon: <GithubToday />,
+    description: "SAT 23: Revert back to normal auth",
+    completion: true,
+    due: "since Friday",
+  },
+]
 
 const formatDate = () => {
   const date = new Date()
@@ -48,16 +70,40 @@ const TodayPage: React.FC = () => {
       <section className="mt-6">
         <TodayTextArea />
       </section>
+
       <section className="space-y-8 text-[16px]">
         <div className="max-w-xs border-b border-[#3A3A3A] opacity-30"></div>
-        <div className="flex items-center justify-start gap-2 text-white">
-          <Square />
-          <span>how to get product market fit </span>
-        </div>
-        <div className="flex items-center justify-start gap-2">
-          <SquareCheck />
-          <span> SAT 23: Revert back to normal auth </span>
-        </div>
+        {todos.map((todo, index) => (
+          <div
+            key={index}
+            className="group flex items-center justify-start gap-2"
+          >
+            {todo.completion ? <CheckedBox /> : <Box />}
+            <p
+              className={`${todo.completion ? "text-gray-color" : "text-white"}`}
+            >
+              {todo.description}
+            </p>
+            <span>{todo.icon}</span>
+            {todo.due && (
+              <div className="flex items-center gap-2">
+                <div className="size-2 rounded-full bg-red-600"></div>{" "}
+                {/* Red dot */}
+                <span>{todo.due}</span>
+              </div>
+            )}
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger className="invisible ml-2 cursor-pointer group-hover:visible">
+                  <Space />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>add to space</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+        ))}
       </section>
     </main>
   )
