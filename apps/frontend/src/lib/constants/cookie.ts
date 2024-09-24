64d554cbc8b0508befb6de1fadf604103fdb6da3
deewakar-k <dakshftw01@gmail.com>
2024-09-24 20:09:13 +0530

    refactor: renamed access token

--- apps/frontend/src/lib/constants/cookie.ts
+++ apps/frontend/src/lib/constants/cookie.ts
@@ -1,14 +1,9 @@
 /**
  * ACCESS_TOKEN cookie name
  */
-export const ACCESS_TOKEN = "__ACCESS_TOKEN__"
-
-//for integration/extensions
-export const MARCH_ACCESS_TOKEN = "__MARCH_ACCESS_TOKEN"
+export const ACCESS_TOKEN = "__MARCH_ACCESS_TOKEN__"
 
 /**
  * REFRESH_TOKEN cookie name
  */
-export const REFRESH_TOKEN = "__REFRESH_TOKEN__"
-
-export const MARCH_REFRESH_TOKEN = "__MARCH_REFRESH__TOKEN"
+export const REFRESH_TOKEN = "__MARCH_REFRESH_TOKEN__"
