8af2130756babbc7d3ea7bd0223d3a2aa68fdaf7
deewakar-k <dakshftw01@gmail.com>
2024-09-24 15:05:58 +0530

    feat: added agenda

--- apps/frontend/src/app/(app)/today/page.tsx
+++ apps/frontend/src/app/(app)/today/page.tsx
@@ -8,6 +8,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/src/components/atoms/Tooltip"
+import { TodayAgenda } from "@/src/components/TodayAgenda"
 import { TodayTextArea } from "@/src/components/TodayTextArea"
 import { Box, CheckedBox } from "@/src/lib/icons/Box"
 import { TodayCal } from "@/src/lib/icons/Calendar"
@@ -48,62 +49,78 @@ const formatDate = () => {
 }
 
 const TodayPage: React.FC = () => {
+  const [showAgenda, setShowAgenda] = React.useState(false)
+
+  const handleToggleAgenda = () => {
+    setShowAgenda((prev) => !prev)
+  }
+
   return (
     <main className="ml-36 text-gray-color">
       <section className=" mt-4 flex max-w-[96%] items-center justify-end gap-4">
         <span className="text-[11px] font-medium text-white">show agenda</span>
-        <ShowAgenda />
-      </section>
-      <section>
-        <header className="flex items-center justify-start gap-4">
-          <TodayCal />
-          <div>
-            <h1 className="text-xl font-medium text-white">Today</h1>
-            <p className="text-sm">{formatDate()}</p>
-          </div>
-          <div className=" ml-20 flex items-center justify-between gap-4">
-            <LeftChevron />
-            <RightChevron />
-          </div>
-        </header>
-      </section>
-      <section className="mt-6">
-        <TodayTextArea />
+        <ShowAgenda toggle={showAgenda} onToggle={handleToggleAgenda} />
       </section>
 
-      <section className="space-y-8 text-[16px]">
-        <div className="max-w-xs border-b border-[#3A3A3A] opacity-30"></div>
-        {todos.map((todo, index) => (
-          <div
-            key={index}
-            className="group flex items-center justify-start gap-2"
-          >
-            {todo.completion ? <CheckedBox /> : <Box />}
-            <p
-              className={`${todo.completion ? "text-gray-color" : "text-white"}`}
-            >
-              {todo.description}
-            </p>
-            <span>{todo.icon}</span>
-            {todo.due && (
-              <div className="flex items-center gap-2">
-                <div className="size-2 rounded-full bg-red-600"></div>{" "}
-                {/* Red dot */}
-                <span>{todo.due}</span>
+      {/* Use flexbox to create two columns */}
+      <section className="mt-6 flex justify-between">
+        <div className="w-2/3">
+          <header className="flex items-center justify-start gap-4">
+            <TodayCal />
+            <div>
+              <h1 className="text-xl font-medium text-white">Today</h1>
+              <p className="text-sm">{formatDate()}</p>
+            </div>
+            <div className="ml-20 flex items-center justify-between gap-4">
+              <LeftChevron />
+              <RightChevron />
+            </div>
+          </header>
+
+          <section className="mt-6">
+            <TodayTextArea />
+          </section>
+
+          <section className="space-y-8 text-[16px]">
+            <div className="max-w-xs border-b border-[#3A3A3A] opacity-30"></div>
+            {todos.map((todo, index) => (
+              <div
+                key={index}
+                className="group flex items-center justify-start gap-2"
+              >
+                {todo.completion ? <CheckedBox /> : <Box />}
+                <p
+                  className={`${todo.completion ? "text-gray-color" : "text-white"}`}
+                >
+                  {todo.description}
+                </p>
+                <span>{todo.icon}</span>
+                {todo.due && (
+                  <div className="flex items-center gap-2">
+                    <div className="size-2 rounded-full bg-red-600"></div>{" "}
+                    {/* Red dot */}
+                    <span>{todo.due}</span>
+                  </div>
+                )}
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger className="invisible ml-2 cursor-pointer group-hover:visible">
+                      <Space />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>add to space</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
               </div>
-            )}
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger className="invisible ml-2 cursor-pointer group-hover:visible">
-                  <Space />
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>add to space</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
+            ))}
+          </section>
+        </div>
+        {showAgenda && (
+          <div className="w-1/4">
+            <TodayAgenda />
           </div>
-        ))}
+        )}
       </section>
     </main>
   )
