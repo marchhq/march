4c5bfe84f61c5679445e1b7275000fe689f6b772
joaorceschini <jrvceschini@gmail.com>
2024-09-26 14:29:27 -0300

    feat: starting inbox page / fix: changed editor font size and color logic

--- apps/frontend/src/app/(app)/inbox/page.tsx
+++ apps/frontend/src/app/(app)/inbox/page.tsx
@@ -1,17 +1,158 @@
 "use client"
 
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 import { Icon } from "@iconify-icon/react"
 import { PlusIcon } from "@radix-ui/react-icons"
 
-import { InboxTextArea } from "@/src/components/InboxTextArea"
+import axios from "axios"
+
+import { useAuth } from "@/src/contexts/AuthContext"
+import TextEditor from "@/src/components/atoms/Editor"
+import useEditorHook from "@/src/hooks/useEditor.hook"
+import useInboxStore from "@/src/lib/store/inbox.store"
+import { BACKEND_URL } from "@/src/lib/constants/urls"
 
 const InboxPage: React.FC = () => {
+  const { session } = useAuth()
+
   const [addingItem, setAddingItem] = useState(false)
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+  const [isSaved, setIsSaved] = useState(false)
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+  const [selectedPages, setSelectedPages] = React.useState<string[]>([])
+  const [editItemId, setEditItemId] = React.useState<string | null>(null)
+  const [editedItem, setEditedItem] = React.useState<{
+    title: string
+    description: string
+  }>({
+    title: "",
+    description: "",
+  })
+
+  const { fetchInboxData, setInboxItems, inboxItems } = useInboxStore()
+
+  const editor = useEditorHook({
+    content: description,
+    setContent: setDescription,
+    setIsSaved,
+    placeholder: "press / for markdown format",
+  })
+
+  const editorEditItem = useEditorHook({
+    content: editedItem.description,
+    setContent: (content) =>
+      setEditedItem((prev) => ({ ...prev, description: content })),
+    setIsSaved,
+    placeholder: "press / for markdown format",
+  })
+
+  useEffect(() => {
+    if (editItemId && editorEditItem) {
+      editorEditItem.commands.setContent(editedItem.description)
+    }
+  }, [editItemId])
+
+  useEffect(() => {
+    const textarea = textareaRef.current
+    if (textarea) {
+      textarea.style.height = "auto"
+      textarea.style.height = `${textarea.scrollHeight}px`
+    }
+  }, [title])
+
+  useEffect(() => {
+    void fetchInboxData(session)
+  }, [fetchInboxData, session, setInboxItems])
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${session}`,
+    },
+  }
+  const handleCloseAddItemToInbox = async () => {
+    setAddingItem(false)
+    setTitle("")
+    setDescription("")
+    editor?.commands.setContent("")
+  }
+
+  const handleEditItem = (item: any) => {
+    setEditItemId(item.uuid)
+    setEditedItem({
+      title: item.title,
+      description: item.description,
+    })
+  }
+
+  const handleCancelEditItem = () => {
+    setEditItemId(null)
+    setEditedItem({ title: "", description: "" })
+  }
+
+  const handleSaveEditedItem = async (item: any) => {
+    console.log(editedItem)
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${session}`,
+        },
+      }
+
+      await axios.put(
+        `${BACKEND_URL}/api/items/${item.uuid}`,
+        editedItem,
+        config
+      )
+
+      void fetchInboxData(session)
+      handleCancelEditItem()
+    } catch (error) {
+      console.error("error updating item:", error)
+    }
+  }
+
+  const handleAddItemToInbox = async () => {
+    if (!session) {
+      console.error("User is not authenticated")
+      return
+    }
+
+    console.log("test")
+
+    try {
+      const res = await axios.post(
+        `${BACKEND_URL}/api/items/create`,
+        {
+          title: title,
+          description: description,
+          dueDate: date,
+          pages: selectedPages,
+        },
+        config
+      )
+
+      if (res.status === 200) {
+        void fetchInboxData(session)
+        editor?.commands.setContent("")
+        setAddingItem(false)
+        setTitle("")
+        setDescription("")
+        console.log("item added")
+        console.log("title", title)
+        console.log("description", description)
+        console.log("dueDate", date)
+        console.log("pages", selectedPages)
+      }
+    } catch (error) {
+      console.error("error adding item to inbox:", error)
+    }
+  }
 
   return (
-    <section className="h-full overflow-auto bg-background ml-[160px] p-16 text-secondary-foreground">
+    <section className="h-full overflow-y-auto bg-background ml-[160px] p-16 text-secondary-foreground">
       <div className="max-w-[800px]">
         <div className="flex flex-col gap-8 text-sm">
           <header className="flex items-center gap-4 text-foreground">
@@ -31,38 +172,105 @@ const InboxPage: React.FC = () => {
               </button>
             ) : (
               <div>
-                <div className="flex justify-end hover-text">
-                  <button onClick={() => setAddingItem(false)}>close</button>
+                <div className="flex justify-end gap-4 text-xs">
+                  <button className="hover-text" onClick={handleAddItemToInbox}>
+                    save
+                  </button>
+                  <button
+                    className="hover-text"
+                    onClick={handleCloseAddItemToInbox}
+                  >
+                    close
+                  </button>
                 </div>
-                <InboxTextArea />
+                <textarea
+                  ref={textareaRef}
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  placeholder="title"
+                  className="w-full py-2 text-2xl font-bold resize-none overflow-hidden bg-background text-foreground placeholder:text-secondary-foreground truncate whitespace-pre-wrap break-words outline-none focus:outline-none"
+                  rows={1}
+                />
+                <TextEditor editor={editor} minH="20vh" />
               </div>
             )}
-            <button className="flex flex-col text-left gap-1 p-4 border border-border rounded-lg hover-bg">
-              <div className="flex items-center gap-2 text-foreground">
-                <Icon
-                  icon="fluent:note-16-regular"
-                  style={{ fontSize: "18px" }}
-                />
-                <p>list of the desired gtms</p>
-              </div>
-              <div className="ml-[18px] pl-2 text-xs">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Consequatur reprehenderit, accusamus nam similique laboriosam
-                  sequi fugit eum veniam nobis itaque eaque fugiat praesentium
-                  vitae minima porro. Temporibus, eligendi deserunt! Cumque!
-                </p>
-              </div>
-            </button>
-            <button className="flex flex-col text-left gap-1 p-4 border border-border rounded-lg hover-bg">
-              <div className="flex items-center gap-2 text-foreground">
-                <Icon icon="ri:github-fill" style={{ fontSize: "18px" }} />
-                <p>Pull request #23</p>
-              </div>
-              <div className="ml-[18px] pl-2 text-xs">
-                <p>webhooks for all exisiting integrations</p>
-              </div>
-            </button>
+            <div className="flex flex-col gap-4">
+              {inboxItems.length === 0 ? (
+                <p>inbox empty</p>
+              ) : (
+                inboxItems.map((item: any) => (
+                  <div
+                    key={item.uuid}
+                    className="flex flex-col text-left gap-1 p-4 border border-border rounded-lg hover-bg"
+                    onClick={() => {
+                      console.log("item.uuid", item.uuid)
+                      console.log("editedItemId", editItemId)
+                    }}
+                  >
+                    <div className="flex justify-between text-foreground">
+                      <div className="w-full flex items-start gap-2">
+                        <Icon
+                          icon="fluent:note-16-regular"
+                          className="mt-0.5"
+                          style={{ fontSize: "18px" }}
+                        />
+                        {editItemId === item.uuid ? (
+                          <div className="w-full">
+                            <textarea
+                              ref={textareaRef}
+                              value={editedItem.title}
+                              onChange={(e) =>
+                                setEditedItem((prev) => ({
+                                  ...prev,
+                                  title: e.target.value,
+                                }))
+                              }
+                              placeholder="title"
+                              className="w-full resize-none overflow-hidden bg-transparent placeholder:text-secondary-foreground truncate whitespace-pre-wrap break-words outline-none focus:outline-none"
+                              rows={1}
+                            />
+                          </div>
+                        ) : (
+                          <p>{item.title}</p>
+                        )}
+                      </div>
+                      <div className="text-secondary-foreground text-xs">
+                        {editItemId === item.uuid ? (
+                          <div className="flex gap-4">
+                            <button
+                              className="hover-text"
+                              onClick={() => handleSaveEditedItem(item)}
+                            >
+                              save
+                            </button>
+                            <button
+                              className="hover-text"
+                              onClick={handleCancelEditItem}
+                            >
+                              cancel
+                            </button>
+                          </div>
+                        ) : (
+                          <button
+                            className="hover-text"
+                            onClick={() => handleEditItem(item)}
+                          >
+                            edit
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                    <div className="ml-[18px] pl-2 text-xs">
+                      {editItemId === item.uuid ? (
+                        <TextEditor editor={editorEditItem} minH="1vh" />
+                      ) : (
+                        <p>{item.description}</p>
+                      )}
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
           </div>
         </div>
       </div>
---
+++ apps/frontend/src/lib/store/inbox.store.ts
@@ -0,0 +1,112 @@
+import axios, { AxiosError } from "axios"
+import { create } from "zustand"
+
+import {
+  InboxItem,
+  InboxStoreType,
+  OverdueInboxItem,
+  TodayInboxItem,
+} from "../@types/Items/Inbox"
+import { BACKEND_URL } from "../constants/urls"
+
+const useInboxStore = create<InboxStoreType>((set) => ({
+  inboxItems: [],
+  todayInboxItems: [],
+  overdueInboxItems: [],
+  isFetched: false,
+  setIsFetched: (isFetched: boolean) => {
+    set({ isFetched })
+  },
+  fetchInboxData: async (session: string) => {
+    let inboxItems_: InboxItem[] = []
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${session}`,
+        },
+      }
+      const { data } = await axios.get(`${BACKEND_URL}/api/my`, config)
+      inboxItems_ = data.response as InboxItem[]
+    } catch (error) {
+      const e = error as AxiosError
+      console.error("Error while fetching my::", e.message)
+    }
+    set({ inboxItems: inboxItems_ })
+    return inboxItems_
+  },
+  setInboxItems: (inboxItems: InboxItem[]) => {
+    // setState for inboxItems
+    set({ inboxItems })
+  },
+  fetchTodayInboxData: async (session: string) => {
+    let todayItems_: TodayInboxItem[] = []
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${session}`,
+        },
+      }
+      const { data } = await axios.get(`${BACKEND_URL}/api/my/today`, config)
+      todayItems_ = data.response.items as TodayInboxItem[]
+    } catch (error) {
+      const e = error as AxiosError
+      console.error("Error while fetching my-today::", e.message)
+    }
+    set({ todayInboxItems: todayItems_ })
+    return todayItems_
+  },
+  setTodayInboxItems: (todayInboxItems: TodayInboxItem[]) => {
+    // setState for todayInboxItems
+    set({ todayInboxItems })
+  },
+  fetchOverdueInboxData: async (session: string) => {
+    let overdueItems_: OverdueInboxItem[] = []
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${session}`,
+        },
+      }
+      const { data } = await axios.get(`${BACKEND_URL}/api/my/overdue`, config) // Fetch overdue items
+      overdueItems_ = data.response.items as OverdueInboxItem[]
+    } catch (error) {
+      const e = error as AxiosError
+      console.error("Error while fetching my-overdue::", e.message)
+    }
+    set({ overdueInboxItems: overdueItems_ })
+    return overdueItems_
+  },
+  setOverdueInboxItems: (overdueInboxItems: OverdueInboxItem[]) => {
+    set({ overdueInboxItems })
+  },
+  moveItemToDate: async (session: string, id: string, date: Date) => {
+    // Update the date of an item
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${session}`,
+        },
+      }
+      const response = await axios.post(
+        `${BACKEND_URL}/api/setDate/`,
+        { id, date: date ? date : null },
+        config
+      )
+      const updatedItem = response.data.response.items
+
+      // Update the inboxItems with the updated item
+      set((state) => ({
+        inboxItems: state.inboxItems.map((item) =>
+          item._id === id ? { ...item, dueDate: date } : item
+        ),
+      }))
+      return updatedItem
+    } catch (error) {
+      const e = error as AxiosError
+      console.error("Error while moving item to new date::", e.message)
+      return null
+    }
+  },
+}))
+
+export default useInboxStore
