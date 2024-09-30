91ddc53a1539d0bf1fa54112ec519375a9095bfa
sajdkabir <sajda.kbir@gmail.com>
2024-09-30 10:54:38 +0530

    style: revome clg

--- apps/backend/src/services/lib/item.service.js
+++ apps/backend/src/services/lib/item.service.js
@@ -173,7 +173,6 @@ const updateItem = async (id, updateData) => {
 };
 
 const moveItemtoDate = async (date, id) => {
-    console.log("date: ", date);
     const formattedDate = date ? new Date(date) : null;
 
     const item = await Item.findByIdAndUpdate(
