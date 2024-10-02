97495a57686cb2195bf1faa7b13a7f1d550e0f90
deewakar <dakshftw01@gmail.com>
2024-10-02 17:49:14 +0530

    fix: dynamic icon

--- apps/frontend/src/components/TodayTextArea.tsx
+++ apps/frontend/src/components/TodayTextArea.tsx
@@ -8,33 +8,37 @@ interface JournalProps {
   selectedDate: Date;
 }
 
-/* const formatDate = (date: Date) => {
+const formatDate = (date: Date) => {
   const isoDate = date.toISOString();
   return isoDate.split("T")[0];
-}; */
+};
 
 export const TodayTextArea = ({ selectedDate }: JournalProps): JSX.Element => {
-  /*
-   const { journal, fetchJournal } = useJournal();
-   const formattedDate = formatDate(selectedDate);
- 
-   useEffect(() => {
-     fetchJournal(formattedDate).then(() => {
-       if (journal?.journal?.content) {
-         setContent(journal?.journal?.content)
-       }
-     });
-   }, [formattedDate, fetchJournal]); */
 
+  const { journal, fetchJournal } = useJournal();
+  const formattedDate = formatDate(selectedDate);
+
+  useEffect(() => {
+    fetchJournal(formattedDate).then(() => {
+      if (journal?.journal?.content) {
+        setContent(journal?.journal?.content)
+      }
+    });
+  }, [formattedDate, fetchJournal]);
+
+  console.log('journal content: ', journal)
+
+  console.log('selected date: ', selectedDate)
 
   const [content, setContent] = useState("<p></p>")
   const [isSaved, setIsSaved] = useState(true);
 
   const editor = useEditorHook({
     content,
-    setContent: () => { },
+    setContent,
     setIsSaved,
   });
+
   return (
     <TextEditor
       editor={editor}
