24c6c3786b141fe80ea7fd4956f3696d108f2198
deewakar <dakshftw01@gmail.com>
2024-10-02 19:08:22 +0530

    feat: get journal based on selected date

--- apps/frontend/src/components/TodayTextArea.tsx
+++ apps/frontend/src/components/TodayTextArea.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import TextEditor from "./atoms/Editor";
 import useEditorHook from "../hooks/useEditor.hook";
 import { useJournal } from "../hooks/useJournal";
@@ -8,33 +8,45 @@ interface JournalProps {
   selectedDate: Date;
 }
 
-/* const formatDate = (date: Date) => {
+const formatDate = (date: Date) => {
   const isoDate = date.toISOString();
   return isoDate.split("T")[0];
-}; */
+};
 
 export const TodayTextArea = ({ selectedDate }: JournalProps): JSX.Element => {
-  /*
-   const { journal, fetchJournal } = useJournal();
-   const formattedDate = formatDate(selectedDate);
- 
-   useEffect(() => {
-     fetchJournal(formattedDate).then(() => {
-       if (journal?.journal?.content) {
-         setContent(journal?.journal?.content)
-       }
-     });
-   }, [formattedDate, fetchJournal]); */
-
-
-  const [content, setContent] = useState("<p></p>")
+  const [content, setContent] = useState("<p></p>");
   const [isSaved, setIsSaved] = useState(true);
+  const formattedDate = formatDate(selectedDate);
+  const { journal, fetchJournal } = useJournal(formattedDate);
+
+  const handleContentChange = useCallback((newContent: string) => {
+    setContent(newContent);
+    setIsSaved(false);
+  }, []);
 
   const editor = useEditorHook({
     content,
-    setContent: () => { },
+    setContent: handleContentChange,
     setIsSaved,
   });
+
+  useEffect(() => {
+    console.log('Fetching journal for date: ', formattedDate);
+    fetchJournal();
+  }, [formattedDate, fetchJournal]);
+
+  useEffect(() => {
+    if (journal?.journal?.content) {
+      console.log('Updating editor content');
+      setContent(journal.journal.content);
+      editor?.commands.setContent(journal.journal.content);
+    } else {
+      console.log('No journal content, resetting editor');
+      setContent("<p></p>");
+      editor?.commands.setContent("<p></p>");
+    }
+  }, [journal, editor]);
+
   return (
     <TextEditor
       editor={editor}
