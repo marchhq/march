77cff9c292473bebd5134ad494c8cafd59e0b186
joaorceschini <jrvceschini@gmail.com>
2024-10-02 13:58:01 -0300

    fix: padronized pages padding

--- apps/frontend/src/app/(app)/space/this-week/page.tsx
+++ apps/frontend/src/app/(app)/space/this-week/page.tsx
@@ -1,5 +1,11 @@
+import React from "react"
+
 const ThisWeekPage: React.FC = () => {
-  return <div className="text-gray-color p-16">this week page</div>
+  return (
+    <div className="size-full overflow-auto bg-background p-16">
+      <p className="text-secondary-foreground">this week page</p>
+    </div>
+  )
 }
 
 export default ThisWeekPage
