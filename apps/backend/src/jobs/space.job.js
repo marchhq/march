c92517415cf57b757c39319e4bd2645925426908
sajdkabir <sajda.kbir@gmail.com>
2024-10-03 09:13:12 +0530

    feat: create block data

--- apps/backend/src/jobs/space.job.js
+++ apps/backend/src/jobs/space.job.js
@@ -1,19 +1,20 @@
 import { Worker } from "bullmq";
 import { redisConnection } from "../loaders/redis.loader.js";
 import { createSpace } from "../services/lib/space.service.js";
+import { createBlock } from "../services/lib/block.service.js";
 
 const processSpaceJob = async (job) => {
     const { user } = job.data;
-    const spaces = [
-        { name: "Notes", icon: "note" },
-        { name: "Meetings", icon: "meeting" },
-        { name: "This Week", icon: "" },
-        { name: "Reading List", icon: "book" }
+    const blocks = [
+        { name: "Notes", data: { type: "notes" } },
+        { name: "Meetings", data: { type: "meetings" } },
+        { name: "This Week", data: { type: "board", filter: { date: ["this-week"] } } },
+        { name: "Reading List", icon: "reading" }
     ];
 
     try {
-        for (const spaceData of spaces) {
-            await createSpace(user, spaceData);
+        for (const blockData of blocks) {
+            await createBlock(user, blockData);
         }
     } catch (error) {
         console.error('Error processing Spaces:', error);
