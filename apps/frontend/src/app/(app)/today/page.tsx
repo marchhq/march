991db86a17a60e9010c860f3602111a05a6c2523
deewakar <dakshftw01@gmail.com>
2024-10-04 23:12:48 +0530

    feat: persist show agenda state accross routes

--- apps/frontend/src/app/(app)/today/page.tsx
+++ apps/frontend/src/app/(app)/today/page.tsx
@@ -1,44 +1,37 @@
 "use client"
-import * as React from "react"
 
+import * as React from "react"
 import { ShowAgenda } from "@/src/components/atoms/ShowAgenda"
 import { TodayTextArea } from "@/src/components/TodayTextArea"
 import { TodayMeetings } from "@/src/components/TodayMeetings"
 import { DateCycle } from "@/src/components/atoms/Date"
 import { TodayItems } from "@/src/components/TodayItems"
 import { DatePicker } from "@/src/components/DatePicker"
+import usePersistedState from "@/src/hooks/usePersistedState"
 
 const TodayPage: React.FC = () => {
-  const [showAgenda, setShowAgenda] = React.useState(false)
+  const [showAgenda, setShowAgenda] = usePersistedState('showAgenda', false);
   const [selectedDate, setSelectedDate] = React.useState(new Date());
 
   const handleToggleAgenda = () => {
-    setShowAgenda((prev) => !prev)
+    setShowAgenda(!showAgenda);
   }
 
   return (
     <main className="h-full overflow-y-auto bg-background ml-[260px]">
-      <section className=" mt-4 flex max-w-[96%] items-center justify-end gap-4">
+      <section className="mt-4 flex max-w-[96%] items-center justify-end gap-4">
         <span className="text-[11px] font-medium text-white">show agenda</span>
         <ShowAgenda toggle={showAgenda} onToggle={handleToggleAgenda} />
       </section>
-
       <section className="mt-6 flex justify-between">
         <div className="w-2/3">
           <header className="flex items-center justify-start gap-4">
-            {/*<span
-              onClick={() => setSelectedDate(new Date())}
-              className="cursor-pointer">
-              <TodayCal />
-            </span> */}
             <DatePicker selectedDate={selectedDate} onDateChange={setSelectedDate} />
             <DateCycle selectedDate={selectedDate} onDateChange={setSelectedDate} />
           </header>
-
           <section className="mt-6 mb-4">
             <TodayTextArea selectedDate={selectedDate} />
           </section>
-
           <section className="space-y-8 text-[16px] text-secondary-foreground">
             <TodayItems selectedDate={selectedDate} />
           </section>
