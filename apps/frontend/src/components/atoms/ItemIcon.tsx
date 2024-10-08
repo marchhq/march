f654e8be00a23f6970465e4a929bfcd4911e2428
joaorceschini <jrvceschini@gmail.com>
2024-10-08 17:46:29 -0300

    feat: this week items ui create/edit & improviments on inbox items

---
+++ apps/frontend/src/components/atoms/ItemIcon.tsx
@@ -0,0 +1,18 @@
+import { Icon } from "@iconify-icon/react"
+
+const iconsMap = {
+  linear: "gg:linear",
+  githubIssue: "ri:github-fill",
+  githubPullRequest: "ri:git-pull-request-line",
+  march: "fluent:note-16-regular",
+  marchClipper: "material-symbols:circle-outline",
+  gmail: "bxl:gmail",
+  sms: "mdi:sms",
+  default: "fluent:note-16-regular",
+}
+
+export const ItemIcon = ({ type }: { type: string }) => {
+  const icon = iconsMap[type] || iconsMap["default"]
+
+  return <Icon icon={icon} style={{ fontSize: "18px" }} className="mt-0.5" />
+}
