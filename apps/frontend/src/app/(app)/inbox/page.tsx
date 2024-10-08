be4ef914d68513c759e9320ac0ecc82c5ea68d03
joaorceschini <jrvceschini@gmail.com>
2024-10-08 14:14:07 -0300

    fix: applying same ui structure for all pages

--- apps/frontend/src/app/(app)/inbox/page.tsx
+++ apps/frontend/src/app/(app)/inbox/page.tsx
@@ -2,7 +2,7 @@ import { InboxPage } from "@/src/components/Inbox/InboxPage"
 
 const Inbox: React.FC = () => {
   return (
-    <section className="h-full overflow-y-auto bg-background ml-[260px] p-16 text-secondary-foreground">
+    <section className="h-full overflow-y-auto bg-background ml-[160px] p-16 text-secondary-foreground">
       <InboxPage />
     </section>
   )
