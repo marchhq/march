b223a18f7ed70e4105f503e251ed09b3a0ae6797
hanzili <hanzili0217@gmail.com>
2024-10-07 21:47:13 -0400

    remove test code

--- apps/frontend/src/app/(app)/profile/page.tsx
+++ apps/frontend/src/app/(app)/profile/page.tsx
@@ -5,12 +5,10 @@ import { useAuth } from "@/src/contexts/AuthContext";
 import useUserStore from "@/src/lib/store/user.store";
 import UserInfo from '@/src/components/profile/UserInfo';
 import Integrations from '@/src/components/profile/Integrations';
-import useLinear from '@/src/hooks/useLinear';
 
 const ProfilePage: React.FC = () => {
   const { session } = useAuth();
   const { user, isLoading, error, fetchUser } = useUserStore();
-  const { fetchMyIssues, issues } = useLinear()
 
   useEffect(() => {
     if (session) {
@@ -18,21 +16,12 @@ const ProfilePage: React.FC = () => {
     }
   }, [session, fetchUser]);
 
-  useEffect(() => {
-    fetchMyIssues()
-  }, [fetchMyIssues])
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div className="text-red-500">{error}</div>;
   if (!user) return <div>User not found</div>;
 
   return (
     <div className="p-6 pt-16 bg-background text-foreground w-1/2 ml-[15%]">
-      {
-        issues.map((issue) => (
-          <div key={issue.id}>{issue.title}</div>
-        ))
-      }
       <UserInfo user={user} />
       <Integrations user={user} />
       <footer className="text-lg text-muted-foreground mt-24">
