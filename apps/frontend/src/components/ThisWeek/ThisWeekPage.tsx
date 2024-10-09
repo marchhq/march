eb06835aaacaf4fb9be3b2fd56fea16bee241644
deewakar <dakshftw01@gmail.com>
2024-10-09 12:28:28 +0530

    feat: filtered week number based on month

--- apps/frontend/src/components/ThisWeek/ThisWeekPage.tsx
+++ apps/frontend/src/components/ThisWeek/ThisWeekPage.tsx
@@ -1,19 +1,42 @@
-import React from "react"
+"use client";
 
-import { ThisWeekArrows } from "@/src/components/ThisWeek/ThisWeekArrows"
-import { ThisWeekSection } from "@/src/components/ThisWeek/ThisWeekSection"
+import React, { useState } from "react";
+import { ThisWeekArrows } from "./ThisWeekArrows";
+import { ThisWeekSection } from "./ThisWeekSection";
+import { addWeeks, startOfMonth } from "date-fns";
+import { getCurrentWeek, getWeeksInMonth } from "@/src/utils/datetime";
 
 export const ThisWeekPage: React.FC = () => {
+  const today = new Date();
+
+  const [currentDate, setCurrentDate] = useState(today);
+
+  const weekNumber = getCurrentWeek(currentDate);
+  const totalWeeks = getWeeksInMonth(currentDate);
+
+  const handleWeekChange = (direction: "left" | "right") => {
+    setCurrentDate((prevDate) => {
+      const newDate = addWeeks(prevDate, direction === "left" ? -1 : 1);
+      const newWeekNumber = getCurrentWeek(newDate);
+
+      if (newWeekNumber < 1 || newWeekNumber > totalWeeks) {
+        return prevDate;
+      }
+
+      return newDate;
+    });
+  };
+
   return (
     <div className="w-9/12 flex flex-col gap-8">
       <div className="flex items-center gap-8 text-sm">
-        <h1 className="text-foreground text-2xl">Week 1</h1>
+        <h1 className="text-foreground text-2xl">Week {weekNumber}</h1>
         <div className="flex gap-4">
           <p>0/6 completed</p>
           <p>0%</p>
           <p>aug 19th - aug 26th</p>
         </div>
-        <ThisWeekArrows />
+        <ThisWeekArrows onChangeWeek={handleWeekChange} />
       </div>
       <div className="flex w-full max-w-screen-xl gap-8">
         <ThisWeekSection icon="material-symbols:circle-outline" title="to do" />
@@ -21,5 +44,5 @@ export const ThisWeekPage: React.FC = () => {
         <ThisWeekSection icon="material-symbols:circle" title="done" />
       </div>
     </div>
-  )
-}
+  );
+};
