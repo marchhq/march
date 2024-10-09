40c294fb3416c60b465f84801627fae921264c8f
joaorceschini <jrvceschini@gmail.com>
2024-10-09 10:10:58 -0300

    fix: overflow-y-auto for inbox items

--- apps/frontend/src/components/Inbox/InboxPage.tsx
+++ apps/frontend/src/components/Inbox/InboxPage.tsx
@@ -5,13 +5,13 @@ import { InboxItems } from "@/src/components/Inbox/InboxItems"
 
 export const InboxPage: React.FC = () => {
   return (
-    <div className="max-w-[800px]">
-      <div className="flex flex-col gap-8 text-sm">
+    <div className="h-full max-w-[800px]">
+      <div className="flex flex-col h-full gap-8 text-sm">
         <header className="flex items-center gap-4 text-foreground">
           <Icon icon="hugeicons:inbox" style={{ fontSize: "38px" }} />
           <h1 className="text-2xl font-semibold">Inbox</h1>
         </header>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col h-full pb-16 gap-4">
           <InboxAddItem />
           <InboxItems />
         </div>
