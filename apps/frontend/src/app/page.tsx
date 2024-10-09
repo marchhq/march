bc80d44d3bc0d2e412b66c9b4bc9e146ddaf5c3b
sajdkabir <sajda.kbir@gmail.com>
2024-10-09 13:43:28 +0530

    added handleKeyDown

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -10,7 +10,6 @@ import Line from "@/src/lib/icons/Line"
 import { LogoDark } from "@/src/lib/icons/Logo"
 import generateMetadata from "@/src/utils/seo"
 
-
 export const metadata: Metadata = generateMetadata({
   path: "/",
   title: "march | satellite",
@@ -44,8 +43,27 @@ const Home: React.FC = () => {
           <div className="mb-10 mt-6 text-center">
             <p>By continuing, you agree to our </p>
             <p>
-              <span className="text-gray-100"><Link target="_blank" href={"https://marchhq.notion.site/Terms-Privacy-67fb3e8525c04fcfa73dca152ecc1dec"}>terms of services</Link></span> and our{" "}
-              <span className="text-gray-100"><Link target="_blank" href={"https://marchhq.notion.site/Terms-Privacy-67fb3e8525c04fcfa73dca152ecc1dec"}>privacy policy.</Link></span>
+              <span className="text-gray-100">
+                <Link
+                  target="_blank"
+                  href={
+                    "https://marchhq.notion.site/Terms-Privacy-67fb3e8525c04fcfa73dca152ecc1dec"
+                  }
+                >
+                  terms of services
+                </Link>
+              </span>{" "}
+              and our{" "}
+              <span className="text-gray-100">
+                <Link
+                  target="_blank"
+                  href={
+                    "https://marchhq.notion.site/Terms-Privacy-67fb3e8525c04fcfa73dca152ecc1dec"
+                  }
+                >
+                  privacy policy.
+                </Link>
+              </span>
             </p>
           </div>
         </div>
