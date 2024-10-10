9a883fc7589686a923d64829b9b2b660eecb1632
joaorceschini <jrvceschini@gmail.com>
2024-10-10 20:07:23 -0300

    fix: fix onboarding ui

--- apps/frontend/src/app/(onboarding)/calendar/page.tsx
+++ apps/frontend/src/app/(onboarding)/calendar/page.tsx
@@ -1,30 +1,35 @@
 import Link from "next/link"
 
+import { Icon } from "@iconify-icon/react"
+
 import CalendarAuth from "@/src/components/atoms/CalendarAuth"
 import CalendarList from "@/src/components/atoms/CalendarList"
-import { GoogleCalendar } from "@/src/lib/icons/Calendar"
 
-const CalendarConnect = (): JSX.Element => {
+const CalendarConnect = () => {
   return (
-    <div className="flex min-h-full flex-col items-center justify-center p-4 text-center text-gray-color">
-      <div className="mt-24 flex flex-col items-center">
-        <GoogleCalendar />
-        <h1 className="mt-10 text-3xl font-bold">Connect Your Calendar</h1>
+    <main className="flex flex-col items-center justify-between h-full">
+      <div className="flex flex-col items-center justify-center gap-8 size-full">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="flex items-center justify-center p-2">
+            <Icon icon="logos:google-calendar" className="text-[50px]" />
+          </div>
+          <h2 className="text-3xl font-bold">Connect Your Calendar</h2>
+        </div>
+        <div className="flex flex-col gap-1 text-base text-secondary-foreground font-medium">
+          <CalendarList />
+        </div>
+        <div className="flex flex-col items-center w-full gap-2 text-base">
+          <CalendarAuth />
+        </div>
       </div>
-      <div className="my-16 flex w-full justify-center">
-        <CalendarList />
+      <div className="w-full text-sm text-secondary-foreground">
+        <div className="text-center">
+          <Link href={"/stack"}>
+            <button className="hover-text">I&apos;ll do this later</button>
+          </Link>
+        </div>
       </div>
-      <div className="mt-16">
-        <CalendarAuth />
-      </div>
-      <div className="mt-44">
-        <Link href={"/stack"}>
-          <button className="hover:text-gray-100">
-            I&apos;ll do this later
-          </button>
-        </Link>
-      </div>
-    </div>
+    </main>
   )
 }
 
--- apps/frontend/src/app/(onboarding)/stack/page.tsx
+++ apps/frontend/src/app/(onboarding)/stack/page.tsx
@@ -1,27 +1,39 @@
+import { Icon } from "@iconify-icon/react"
 import React from "react"
-
 import Link from "next/link"
 
 import { Integrations } from "@/src/components/atoms/Integrations"
 
 const StackConnect: React.FC = () => {
   return (
-    <div className="flex min-h-full flex-col items-center justify-center p-4 text-center text-gray-color">
-      <div className="mt-24 flex flex-col items-center">
-        <h1 className="text-2xl font-semibold">Connect Your Stack</h1>
+    <main className="flex flex-col items-center justify-between h-full">
+      <div className="flex flex-col items-center justify-center gap-16 size-full">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="flex items-center justify-center p-2">
+            <Icon icon="uil:circuit" className="text-[50px]" />
+          </div>
+          <h2 className="text-3xl font-bold">Connect Your Stack</h2>
+        </div>
+        <div className="flex flex-col gap-1 text-base text-secondary-foreground font-medium">
+          <Integrations />
+        </div>
+        <div className="flex flex-col items-center w-full gap-2 text-base">
+          <Link
+            href={"/today"}
+            className="flex gap-x-6 bg-transparent p-6 text-xl font-semibold hover-text"
+          >
+            continue
+          </Link>
+        </div>
       </div>
-      <div className="my-16 mt-12">
-        <Integrations />
+      <div className="w-full text-sm text-secondary-foreground">
+        <div className="text-center">
+          <Link href={"/inbox"}>
+            <button className="hover-text">I&apos;ll do this later</button>
+          </Link>
+        </div>
       </div>
-      <div className="mt-14 space-y-4">
-        <Link href={"/today"}>
-          <button className="flex w-60 items-center justify-center gap-x-6 bg-transparent p-6 text-xl font-semibold hover:text-gray-100">
-            Continue
-          </button>
-        </Link>
-        <button className="hover:text-gray-100">i&apos;ll do this later</button>
-      </div>
-    </div>
+    </main>
   )
 }
 
