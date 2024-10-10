650faaa02df0ffe17e5d179646106fe8b6ef8566
deewakar <dakshftw01@gmail.com>
2024-10-10 15:13:11 +0530

    feat: create and get item on click outsite, removed icon

--- apps/frontend/src/components/ThisWeek/ThisWeekPage.tsx
+++ apps/frontend/src/components/ThisWeek/ThisWeekPage.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { ThisWeekArrows } from "./ThisWeekArrows"
 import { ThisWeekSection } from "./ThisWeekSection"
 import { addWeeks } from "date-fns"
@@ -9,6 +9,9 @@ import {
   getFormattedDateRange,
   getWeeksInMonth,
 } from "@/src/utils/datetime"
+import { useAuth } from "@/src/contexts/AuthContext"
+import useItemsStore, { ItemStoreType } from "@/src/lib/store/items.store"
+import { Item } from "@/src/lib/@types/Items/Items"
 
 export const ThisWeekPage: React.FC = () => {
   const today = new Date()
@@ -16,6 +19,19 @@ export const ThisWeekPage: React.FC = () => {
   const weekNumber = getCurrentWeek(currentDate)
   const totalWeeks = getWeeksInMonth(currentDate)
   const formattedDateRange = getFormattedDateRange(currentDate)
+
+  const { session } = useAuth()
+  const fetchItems = useItemsStore((state: ItemStoreType) => state.fetchItems)
+  const items = useItemsStore((state: ItemStoreType) => state.items)
+
+  useEffect(() => {
+    const filter = "this-week"
+    fetchItems(session, filter)
+  }, [fetchItems])
+
+  const todo = items.filter((item: Item) => item.status === "todo")
+  const inProgress = items.filter((item: Item) => item.status === "in progress")
+  const done = items.filter((item: Item) => item.status === "done")
 
   const handleWeekChange = (direction: "left" | "right") => {
     setCurrentDate((prevDate) => {
@@ -32,16 +48,30 @@ export const ThisWeekPage: React.FC = () => {
       <div className="flex items-center gap-8 text-sm">
         <h1 className="text-foreground text-2xl">Week {weekNumber}</h1>
         <div className="flex gap-4">
-          <p>0/6 completed</p>
-          <p>0%</p>
+          <p>
+            {done.length}/{items.length} completed
+          </p>
+          <p>{((done.length / items.length) * 100).toFixed(0)}%</p>
           <p>{formattedDateRange}</p>
         </div>
         <ThisWeekArrows onChangeWeek={handleWeekChange} />
       </div>
       <div className="flex w-full max-w-screen-xl gap-8">
-        <ThisWeekSection icon="material-symbols:circle-outline" title="to do" />
-        <ThisWeekSection icon="carbon:circle-dash" title="in progress" />
-        <ThisWeekSection icon="material-symbols:circle" title="done" />
+        <ThisWeekSection
+          icon="material-symbols:circle-outline"
+          title="to do"
+          items={todo}
+        />
+        <ThisWeekSection
+          icon="carbon:circle-dash"
+          title="in progress"
+          items={inProgress}
+        />
+        <ThisWeekSection
+          icon="material-symbols:circle"
+          title="done"
+          items={done}
+        />
       </div>
     </div>
   )
