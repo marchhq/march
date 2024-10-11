944b952cce0b09c92308ab7f672a0c04746a551e
deewakar <dakshftw01@gmail.com>
2024-10-11 09:09:46 +0530

    fix: eslint

--- apps/frontend/src/app/(app)/space/meetings/[meetingId]/page.tsx
+++ apps/frontend/src/app/(app)/space/meetings/[meetingId]/page.tsx
@@ -1,15 +1,15 @@
 import MeetingPage from "@/src/components/meetings/MeetingsPage"
 
-export default function MeetingsPage({ params }: {
+export default function MeetingsPage({
+  params,
+}: {
   params: {
     meetingId?: string
   }
 }) {
   if (!params.meetingId) {
-    return console.log("meetId is undefined");
+    return console.log("meetId is undefined")
   }
 
-  return (
-    <MeetingPage meetId={params.meetingId} />
-  )
+  return <MeetingPage meetId={params.meetingId} />
 }
