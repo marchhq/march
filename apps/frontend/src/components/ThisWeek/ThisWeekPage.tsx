b04c07d921610994d8112e2d013f37d0020afc17
joaorceschini <jrvceschini@gmail.com>
2024-10-11 15:18:29 -0300

    feat: this week expanded item

--- apps/frontend/src/components/ThisWeek/ThisWeekPage.tsx
+++ apps/frontend/src/components/ThisWeek/ThisWeekPage.tsx
@@ -6,8 +6,10 @@ import { addWeeks } from "date-fns"
 
 import { CustomKanban } from "./CustomKanban"
 import { ThisWeekArrows } from "./ThisWeekArrows"
+import { ThisWeekExpandedItem } from "@/src/components/ThisWeek/ThisWeekExpandedItem"
 import { Item } from "@/src/lib/@types/Items/Items"
 import useItemsStore from "@/src/lib/store/items.store"
+import classNames from "@/src/utils/classNames"
 import {
   getCurrentWeek,
   getFormattedDateRange,
@@ -20,7 +22,7 @@ export const ThisWeekPage: React.FC = () => {
   const weekNumber = getCurrentWeek(currentDate)
   const totalWeeks = getWeeksInMonth(currentDate)
   const formattedDateRange = getFormattedDateRange(currentDate)
-  const { items } = useItemsStore()
+  const { items, selectedItem } = useItemsStore()
 
   const doneItems = items.filter((item: Item) => item.status === "done")
 
@@ -35,24 +37,32 @@ export const ThisWeekPage: React.FC = () => {
   }
 
   return (
-    <div className="flex w-9/12 flex-col gap-8">
-      <div className="flex items-center gap-8 text-sm">
-        <h1 className="text-2xl text-foreground">Week {weekNumber}</h1>
-        <div className="flex gap-4">
-          <p>
-            {doneItems.length}/{items.length} completed
-          </p>
-          <p>
-            {items.length > 0
-              ? ((doneItems.length / items.length) * 100).toFixed(0)
-              : 0}
-            %
-          </p>
-          <p>{formattedDateRange}</p>
+    <div className="flex size-full">
+      <div
+        className={classNames(
+          "flex flex-col gap-8",
+          selectedItem ? "w-1/2" : "w-full lg:w-9/12"
+        )}
+      >
+        <div className="flex items-center gap-8 text-sm">
+          <h1 className="text-2xl text-foreground">Week {weekNumber}</h1>
+          <div className="flex gap-4">
+            <p>
+              {doneItems.length}/{items.length} completed
+            </p>
+            <p>
+              {items.length > 0
+                ? ((doneItems.length / items.length) * 100).toFixed(0)
+                : 0}
+              %
+            </p>
+            <p>{formattedDateRange}</p>
+          </div>
+          <ThisWeekArrows onChangeWeek={handleWeekChange} />
         </div>
-        <ThisWeekArrows onChangeWeek={handleWeekChange} />
+        <CustomKanban />
       </div>
-      <CustomKanban />
+      <ThisWeekExpandedItem />
     </div>
   )
 }
