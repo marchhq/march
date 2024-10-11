ad0e11b381c4a4966ee1f6477cafe51d97b0cbf9
joaorceschini <jrvceschini@gmail.com>
2024-10-11 12:20:15 -0300

    fix: this week ui

--- apps/frontend/src/components/ThisWeek/CustomKanban.tsx
+++ apps/frontend/src/components/ThisWeek/CustomKanban.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useCallback, useEffect, useRef, useState } from "react"
 
 import { Icon } from "@iconify-icon/react"
 import { motion } from "framer-motion"
@@ -29,21 +29,21 @@ const Board = () => {
   return (
     <div className="flex size-full gap-3 overflow-scroll p-12">
       <Column
-        title="TODO"
+        title="todo"
         column="todo"
         items={items.filter((item) => item.status === "todo")}
         onDragEnd={handleDragEnd}
         icon="material-symbols:circle-outline"
       />
       <Column
-        title="In progress"
+        title="in progress"
         column="in progress"
         items={items.filter((item) => item.status === "in progress")}
         onDragEnd={handleDragEnd}
         icon="carbon:circle-dash"
       />
       <Column
-        title="Done"
+        title="done"
         column="done"
         items={items.filter((item) => item.status === "done")}
         onDragEnd={handleDragEnd}
@@ -134,7 +134,7 @@ const Column = ({ title, items, column, onDragEnd, icon }) => {
         onDrop={handleDragEnd}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
-        className={`size-full transition-colors`}
+        className={`size-full`}
       >
         {items.map((item) => (
           <Card key={item._id} {...item} handleDragStart={handleDragStart} />
@@ -155,7 +155,7 @@ const Card = ({ title, _id, status, handleDragStart }) => {
         layoutId={_id}
         draggable="true"
         onDragStart={(e) => handleDragStart(e, { _id, status })}
-        className="hover-bg group flex cursor-grab flex-col gap-1 rounded-lg border-border p-4 text-left hover:border active:cursor-grabbing"
+        className="group flex cursor-grab flex-col gap-1 rounded-lg border border-transparent p-4 text-left hover:border-border active:cursor-grabbing"
       >
         <p className="text-sm text-neutral-100">{title}</p>
       </motion.div>
@@ -168,7 +168,7 @@ const DropIndicator = ({ beforeId, column }) => {
     <div
       data-before={beforeId || "-1"}
       data-column={column}
-      className="my-0.5 h-0.5 w-full bg-primary-foreground opacity-0"
+      className="h-px w-full bg-secondary-foreground opacity-0"
     />
   )
 }
@@ -198,20 +198,23 @@ const AddCard: React.FC<AddCardProps> = ({ column, addItem }) => {
     }
   }, [text])
 
-  const handleSubmit = (e?: React.FormEvent) => {
-    e?.preventDefault()
-    if (!text.trim().length) return
+  const handleSubmit = useCallback(
+    (e?: React.FormEvent) => {
+      e?.preventDefault()
+      if (!text.trim().length) return
 
-    console.log("Attempting to add item:", {
-      session,
-      dueDate: new Date().toISOString(),
-      title: text.trim(),
-      status: column,
-    })
+      console.log("Attempting to add item:", {
+        session,
+        dueDate: new Date().toISOString(),
+        title: text.trim(),
+        status: column,
+      })
 
-    addItem(session, new Date().toISOString(), text.trim(), column)
-    handleCancel()
-  }
+      addItem(session, new Date().toISOString(), text.trim(), column)
+      handleCancel()
+    },
+    [addItem, column, session, text]
+  )
 
   const handleCancel = () => {
     setText("")
@@ -237,7 +240,7 @@ const AddCard: React.FC<AddCardProps> = ({ column, addItem }) => {
     return () => {
       document.removeEventListener("mousedown", handleClickOutside)
     }
-  }, [adding, text])
+  }, [adding, text, handleSubmit])
 
   return (
     <div ref={addItemRef}>
@@ -249,7 +252,6 @@ const AddCard: React.FC<AddCardProps> = ({ column, addItem }) => {
             onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
               setText(e.target.value)
             }
-            autoFocus
             placeholder="title"
             className="w-full resize-none overflow-hidden truncate whitespace-pre-wrap break-words bg-transparent py-1 text-base font-bold text-foreground outline-none placeholder:text-secondary-foreground focus:outline-none"
             rows={1}
@@ -260,7 +262,7 @@ const AddCard: React.FC<AddCardProps> = ({ column, addItem }) => {
         <motion.button
           layout
           onClick={() => setAdding(true)}
-          className="hover-bg flex w-full items-center gap-2 rounded-lg p-4 text-sm transition-colors"
+          className="hover-bg flex w-full items-center gap-2 rounded-lg p-4 text-sm"
         >
           <Icon icon="ic:round-plus" className="text-[18px]" />
           <p>New item</p>
