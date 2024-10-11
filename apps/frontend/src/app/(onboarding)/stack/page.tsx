b077659e8b55ff45c02e7043e7b23156735288a4
joaorceschini <jrvceschini@gmail.com>
2024-10-11 09:38:39 -0300

    fix: fixed gap on stack page

--- apps/frontend/src/app/(onboarding)/stack/page.tsx
+++ apps/frontend/src/app/(onboarding)/stack/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react"
 
 import { Icon } from "@iconify-icon/react"
@@ -8,23 +10,23 @@ import { Integrations } from "@/src/components/atoms/Integrations"
 const StackConnect: React.FC = () => {
   return (
     <main className="flex h-full flex-col items-center justify-between">
-      <div className="flex size-full flex-col items-center justify-center gap-16">
+      <div className="flex size-full flex-col items-center justify-center gap-12 pb-12">
         <div className="flex flex-col items-center justify-center gap-4">
           <div className="flex items-center justify-center p-2">
             <Icon icon="uil:circuit" className="text-[50px]" />
           </div>
           <h2 className="text-3xl font-bold">Connect Your Stack</h2>
         </div>
-        <div className="flex flex-col gap-1 text-base font-medium text-secondary-foreground">
+        <div className="flex flex-col gap-8 text-base font-medium text-secondary-foreground">
           <Integrations />
-        </div>
-        <div className="flex w-full flex-col items-center gap-2 text-base">
-          <Link
-            href={"/today"}
-            className="hover-text flex gap-x-6 bg-transparent p-6 text-xl font-semibold"
-          >
-            continue
-          </Link>
+          <div className="flex w-full flex-col items-center text-base">
+            <Link
+              href={"/today"}
+              className="hover-text flex gap-x-6 bg-transparent text-xl font-semibold"
+            >
+              Continue
+            </Link>
+          </div>
         </div>
       </div>
       <div className="w-full text-sm text-secondary-foreground">
