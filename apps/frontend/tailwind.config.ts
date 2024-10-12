1aba1d54b7eab46525923c4f9edc0a62cfca9426
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-10-12 09:04:49 +0530

    Revert "[ui] add loading screen with a message in on-boarding last step"
    This reverts commit 0029dbaeae962900289dbdfb767b27cabe77d680.

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -154,16 +154,6 @@ const config: Config = {
             transform: "translate(1px, -2px) rotate(-1deg)",
           },
         },
-        pulsate: {
-          "0%": {
-            transform: "scale(1)",
-            opacity: "0.1",
-          },
-          "100%": {
-            transform: "scale(1.2)",
-            opacity: "1",
-          },
-        },
       },
       animation: {
         fadeIn: "fadeIn 400ms cubic-bezier(0.16, 1, 0.3, 1)",
@@ -182,7 +172,6 @@ const config: Config = {
         scale: "scale 150ms cubic-bezier(0.4, 0, 0.2, 1)",
         rise: "rise 150ms cubic-bezier(0.4, 0, 0.2, 1)",
         shake: "shake 150ms cubic-bezier(0.4, 0, 0.2, 1)",
-        pulsate: "pulsate 2000ms ease-in-out infinite",
       },
       colors: {
         foreground: "rgba(var(--foreground))",
