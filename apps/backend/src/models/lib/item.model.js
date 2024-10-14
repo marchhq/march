3433512844596348e85d0959febdade9edd8585a
deewakar <dakshftw01@gmail.com>
2024-10-14 21:20:18 +0530

    format

--- apps/backend/src/models/lib/item.model.js
+++ apps/backend/src/models/lib/item.model.js
@@ -5,94 +5,94 @@ import { db } from "../../loaders/db.loader.js";
 const statusChoices = ["null", "todo", "in progress", "done", "archive"];
 
 const ItemSchema = new Schema(
-  {
-    uuid: {
-      type: String,
-      default: () => uuid(),
+    {
+        uuid: {
+            type: String,
+            default: () => uuid()
+        },
+        title: {
+            type: String
+        },
+        type: {
+            type: String,
+            default: "Issue"
+        },
+        source: {
+            type: String,
+            default: "march"
+        },
+        description: {
+            type: String,
+            default: ""
+        },
+        dueDate: {
+            type: Date,
+            default: null
+        },
+        status: {
+            type: String,
+            enum: statusChoices,
+            default: "null"
+        },
+        id: {
+            type: String
+        },
+        metadata: {
+            type: Schema.Types.Mixed
+        },
+        createdAt: {
+            type: Date
+        },
+        updatedAt: {
+            type: Date
+        },
+        spaces: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Space"
+            }
+        ],
+        blocks: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Block"
+            }
+        ],
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        labels: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Label"
+            }
+        ],
+        isCompleted: {
+            type: Boolean,
+            default: false
+        },
+        isArchived: {
+            type: Boolean,
+            default: false
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false
+        }
     },
-    title: {
-      type: String,
-    },
-    type: {
-      type: String,
-      default: "Issue",
-    },
-    source: {
-      type: String,
-      default: "march",
-    },
-    description: {
-      type: String,
-      default: "",
-    },
-    dueDate: {
-      type: Date,
-      default: null,
-    },
-    status: {
-      type: String,
-      enum: statusChoices,
-      default: "null",
-    },
-    id: {
-      type: String,
-    },
-    metadata: {
-      type: Schema.Types.Mixed,
-    },
-    createdAt: {
-      type: Date,
-    },
-    updatedAt: {
-      type: Date,
-    },
-    spaces: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Space",
-      },
-    ],
-    blocks: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Block",
-      },
-    ],
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    labels: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Label",
-      },
-    ],
-    isCompleted: {
-      type: Boolean,
-      default: false,
-    },
-    isArchived: {
-      type: Boolean,
-      default: false,
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  {
-    timestamps: true,
-  },
+    {
+        timestamps: true
+    }
 );
 
 ItemSchema.pre("save", function (next) {
-  if (this.status === "done") {
-    this.isCompleted = true;
-  } else {
-    this.isCompleted = false;
-  }
-  next();
+    if (this.status === "done") {
+        this.isCompleted = true;
+    } else {
+        this.isCompleted = false;
+    }
+    next();
 });
 
 const Item = db.model("Item", ItemSchema, "items");
