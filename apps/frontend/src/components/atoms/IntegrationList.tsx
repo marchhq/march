45e2783d5d76f2bea675cd6d94e9a504ec2fa994
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-10-14 22:12:45 +0530

    Update IntegrationList.tsx

--- apps/frontend/src/components/atoms/IntegrationList.tsx
+++ apps/frontend/src/components/atoms/IntegrationList.tsx
@@ -7,7 +7,7 @@ import { Cal } from "@/src/lib/icons/Calendar"
 import { GithubDark } from "@/src/lib/icons/Github"
 import { LinearDark } from "@/src/lib/icons/LinearCircle"
 import { NotionDark } from "@/src/lib/icons/Notion"
-
+import  useGithubLogin  from "@/src/hooks/useGithubLogin"
 const integrations = [
   {
     key: "googleCalendar",
@@ -42,7 +42,18 @@ const integrations = [
 export const IntegrationList = (): JSX.Element => {
   const user = useUserInfo()
   const handleLogin = useGoogleCalendarLogin("/profile")
+  const handleGitHubLogin = useGithubLogin();
 
+   const handleIntegrationLogin = (integrationKey: string) => {
+    if (integrationKey === "googleCalendar") {
+      return handleLogin;
+    } else if (integrationKey === "github") {
+      return handleGitHubLogin; 
+    } else {
+      return () => {}; \
+    }
+  };
+  
   return (
     <div className=" space-y-4">
       {integrations.map((integration) => {
@@ -74,7 +85,7 @@ export const IntegrationList = (): JSX.Element => {
             ) : (
               <button
                 onClick={
-                  integration.key == "googleCalendar" ? handleLogin : () => {}
+                 handleIntegrationLogin(integration.key)
                 }
                 className="flex items-center text-sm"
               >
