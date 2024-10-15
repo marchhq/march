281d9ec78015237a6e75870f7b1cb4955de6b964
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-10-16 00:26:39 +0530

    Update github.route.js

--- apps/backend/src/routers/integration/github.route.js
+++ apps/backend/src/routers/integration/github.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { githubAccessTokenController, handleGithubCallbackController } from "../../controllers/integration/github.controller.js";
+import {  handleGithubCallbackController } from "../../controllers/integration/github.controller.js";
 
 const router = Router();
 
 router.get('/callback/', handleGithubCallbackController);
-router.route("/getAccessToken/").get(githubAccessTokenController)
+
 export default router;
