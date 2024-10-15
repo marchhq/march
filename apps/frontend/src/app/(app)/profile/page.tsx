b709d57af7dfdbfa61258756564a50fe58277072
Madhav <madhavsingh.relish@gmail.com>
2024-10-16 00:08:39 +0530

    Added logout button

--- apps/frontend/src/app/(app)/profile/page.tsx
+++ apps/frontend/src/app/(app)/profile/page.tsx
@@ -6,10 +6,14 @@ import Integrations from "@/src/components/profile/Integrations"
 import UserInfo from "@/src/components/profile/UserInfo"
 import { useAuth } from "@/src/contexts/AuthContext"
 import useUserStore from "@/src/lib/store/user.store"
+import { Spinner } from "@phosphor-icons/react"
+import { Loader2, LogOutIcon, LucideLoader2 } from "lucide-react"
 
 const ProfilePage: React.FC = () => {
   const { session } = useAuth()
   const { user, isLoading, error, fetchUser } = useUserStore()
+  const { logout } = useAuth()
+  const [isLoggingOut, setIsLoggingOut] = React.useState(false)
 
   useEffect(() => {
     if (session) {
@@ -20,6 +24,12 @@ const ProfilePage: React.FC = () => {
   if (isLoading) return <div>Loading...</div>
   if (error) return <div className="text-red-500">{error}</div>
   if (!user) return <div>User not found</div>
+
+    const handleLogout = async()=>{
+      setIsLoggingOut(true)
+      await logout()
+      setIsLoggingOut(false)
+    }
 
   return (
     // TODO:: Can have a better way to adjust the layout(left margin) using flex
@@ -32,6 +42,18 @@ const ProfilePage: React.FC = () => {
           — crafted for the makers to{" "}
           <span className="text-primary-foreground">get things done</span>;
         </p>
+        <button
+          onClick={handleLogout}
+          disabled={isLoggingOut}
+          className="px-2 py-1 mt-12 flex gap-2 items-center rounded-lg text-base font-semibold text-[#EF6258CC] bg-[#382826E5]"
+        >
+          Logout{" "}
+          {!isLoggingOut ? (
+            <LucideLoader2 size={20} />
+          ) : (
+            <LogOutIcon size={20} />
+          )}
+        </button>
       </footer>
     </div>
   )
