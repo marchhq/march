becb013d2161b78932c60d9c5e0e6ab09675cafd
deewakar <dakshftw01@gmail.com>
2024-10-16 12:59:58 +0530

    added cycle items store

---
+++ apps/frontend/src/lib/@types/Items/Cycle.ts
@@ -0,0 +1,32 @@
+export interface CycleItem {
+  cycleDate: string
+  _id: string
+  title: string
+  type: string
+  source: string
+  description: string
+  dueDate: string
+  status: string
+  spaces: string[]
+  blocks: string[]
+  labels: string[]
+  isCompleted: boolean
+  isArchived: boolean
+  isDeleted: boolean
+  uuid: string
+}
+
+export interface CycleItems {
+  response: CycleItem[]
+}
+
+export interface CycleItemStoreTypes {
+  item: CycleItem | null
+  items: CycleItem[]
+  isLoading: boolean
+  isFetched: boolean
+  fetchItems: (session: string) => Promise<void>
+  createItem: (data: CycleItem, session: string) => Promise<void>
+  setItem: (item: CycleItem | null) => void
+  setIsFetched: (isFetched: boolean) => void
+}
