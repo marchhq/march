690981140a9687106bc575276f74bb9194611234
joaorceschini <jrvceschini@gmail.com>
2024-10-16 22:01:49 -0300

    notes page

--- apps/frontend/src/app/(app)/space/notes/page.tsx
+++ apps/frontend/src/app/(app)/space/notes/page.tsx
@@ -1,12 +1,29 @@
 "use client"
 
-import React, { useEffect } from "react"
+import React, { useEffect, useCallback } from "react"
 
+import { useAuth } from "@/src/contexts/AuthContext"
 import { redirectNote } from "@/src/lib/server/actions/redirectNote"
 import useNotesStore from "@/src/lib/store/notes.store"
 
 const NotesPage: React.FC = () => {
-  const { latestNote } = useNotesStore()
+  const { session } = useAuth()
+  const { latestNote, fetchNotes, setIsFetched, isFetched } = useNotesStore()
+
+  const fetchTheNotes = useCallback(async (): Promise<void> => {
+    try {
+      await fetchNotes(session)
+      setIsFetched(true)
+    } catch (error) {
+      setIsFetched(false)
+    }
+  }, [session, fetchNotes, setIsFetched])
+
+  useEffect(() => {
+    if (!isFetched) {
+      fetchTheNotes()
+    }
+  }, [fetchTheNotes, isFetched])
 
   useEffect(() => {
     if (latestNote) {
