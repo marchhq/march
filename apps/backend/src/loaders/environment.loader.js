fd9aa8142c382eac44ee97b91ad28dfeb52a2f6a
sajdkabir <sajda.kbir@gmail.com>
2024-10-18 14:45:11 +0530

    db url

--- apps/backend/src/loaders/environment.loader.js
+++ apps/backend/src/loaders/environment.loader.js
@@ -3,10 +3,7 @@ import { config } from "dotenv";
 config();
 
 export const environment = {
-    DB_USER: process.env.DB_USER,
-    DB_PASS: process.env.DB_PASS,
-    DB_NAME: process.env.DB_NAME,
-    DB_HOST: process.env.DB_HOST,
+    MONGO_URL: process.env.MONGO_URL,
     PORT: process.env.PORT || 8080,
     JWT_ISSUER: process.env.JWT_ISSUER,
     JWT_SECRET: process.env.JWT_SECRET,
