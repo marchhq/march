9e9d10fb204d59f55aad5a010f00dde4d0b95b8b
sajdkabir <sajda.kbir@gmail.com>
2024-10-23 22:10:35 +0530

    fix url issue

--- apps/frontend/src/lib/constants/urls.ts
+++ apps/frontend/src/lib/constants/urls.ts
@@ -1,12 +1,11 @@
-// const LOCAL_BACKEND = "http://localhost:8080"
+const LOCAL_BACKEND = "http://localhost:8080"
 
-// const LOCAL_FRONTEND = "http://localhost:3000"
+const LOCAL_FRONTEND = "http://localhost:3000"
 
-// const ENV_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+const ENV_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
-// const ENV_FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL
+const ENV_FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+export const BACKEND_URL = ENV_BACKEND_URL ?? LOCAL_BACKEND
 
-export const FRONTEND_URL = "https://app.march.cat"
-console.log("env check: ", process.env.NEXT_PUBLIC_FRONTEND_URL)
+export const FRONTEND_URL = ENV_FRONTEND_URL ?? LOCAL_FRONTEND
