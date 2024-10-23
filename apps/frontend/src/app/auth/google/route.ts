f89c09dffdd47b130e274335a51ba27377bd7453
sajdkabir <sajda.kbir@gmail.com>
2024-10-23 20:58:25 +0530

    test again

--- apps/frontend/src/app/auth/google/route.ts
+++ apps/frontend/src/app/auth/google/route.ts
@@ -110,8 +110,8 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
 //   }
 
 //   // redirect domain based on the current environment
-//   const isLocal = request.nextUrl.hostname.includes("localhost")
-//   const redirectDomain = "https://app.march.cat"
+// const isLocal = request.nextUrl.hostname.includes("localhost")
+// const redirectDomain = "https://app.march.cat"
 
 //   const response = NextResponse.redirect(
 //     new URL(res.isNewUser ? "/calendar" : "/today", redirectDomain)
