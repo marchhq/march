275aa5d365f3559791df95dad4ef88f76cb7d98e
madhav-relish <madhavsingh.relish@gmail.com>
2024-10-23 12:44:31 +0530

    Changed the admin panel port and waitlist page design

--- apps/frontend/src/app/(waitlist)/waitlist/page.tsx
+++ apps/frontend/src/app/(waitlist)/waitlist/page.tsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-import waitlist from '../../../../public/icons/waitlist.svg'
+import waitlist from '../../../../public/icons/logo.svg'
+import { LogoDark } from '@/src/lib/icons/Logo'
 
 type Props = {}
 
 const Waitlist = (props: Props) => {
   return (
-    <div className='flex flex-col justify-center items-center h-screen bg-background'>
-      <Image 
-      src={waitlist}
-        alt="Waitlist" 
-        width={60}
-        height={60}
-        className="absolute inset-0 object-cover w-full h-full opacity-30" 
-      />
-      <div className='relative z-10 p-6 bg-white rounded-lg shadow-lg max-w-md text-center'>
-        <h3 className='text-2xl font-semibold text-secondary-foreground mb-4'>
-          Thank You for Registering with March!
-        </h3>
-        <p className='text-lg text-gray-700'>
-          Sit back and keep an eye on your email. You will receive your confirmation after we review your profile.
-        </p>
-      </div>
+    <div className='flex flex-col justify-center items-center gap-4 h-screen bg-background text-primary-foreground'>
+    {/* <Image
+    src={waitlist}
+    width={70}
+    height={70}
+    alt='March Logo'
+    /> */}
+    <LogoDark/>
+    <h3 className='text-sm'>your flight to mars has been confirmed.</h3>
+    <p className='ml-20 text-base text-secondary-foreground'> — we’ll get back to you soon;</p>
     </div>
 
     
