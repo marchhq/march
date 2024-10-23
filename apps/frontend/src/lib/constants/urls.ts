9398abeb426dac5c50aca0786ce0595c381b3249
sajdkabir <sajda.kbir@gmail.com>
2024-10-23 20:50:25 +0530

    test

--- apps/frontend/src/lib/constants/urls.ts
+++ apps/frontend/src/lib/constants/urls.ts
@@ -1,11 +1,11 @@
-const LOCAL_BACKEND = "http://localhost:8080"
+// const LOCAL_BACKEND = "http://localhost:8080"
 
-const LOCAL_FRONTEND = "http://localhost:3000"
+// const LOCAL_FRONTEND = "http://localhost:3000"
 
-const ENV_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+// const ENV_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
-const ENV_FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL
+// const ENV_FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL
 
-export const BACKEND_URL = ENV_BACKEND_URL ?? LOCAL_BACKEND
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
-export const FRONTEND_URL = ENV_FRONTEND_URL ?? LOCAL_FRONTEND
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL
