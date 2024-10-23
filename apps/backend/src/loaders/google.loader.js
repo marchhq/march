b67f41c770fa80d066c0a6cc6e93c6c07d232127
sajdkabir <sajda.kbir@gmail.com>
2024-10-23 10:15:42 +0530

    refactor: google loaders

--- apps/backend/src/loaders/google.loader.js
+++ apps/backend/src/loaders/google.loader.js
@@ -2,17 +2,15 @@ import { OAuth2Client } from "google-auth-library";
 import { environment } from "./environment.loader.js";
 
 const client = new OAuth2Client(
-  environment.GOOGLE_CLIENT_ID,
-  environment.GOOGLE_CLIENT_SECRET,
-  //environment.GOOGLE_REDIRECT_URL
-  environment.GOOGLE_REDIRECT_URL ||
-    "http://localhost:3000/auth/google-calendar",
+    environment.GOOGLE_CLIENT_ID,
+    environment.GOOGLE_CLIENT_SECRET,
+    environment.GOOGLE_REDIRECT_URL
 );
 
 const OauthCalClient = new OAuth2Client(
-  environment.GOOGLE_CLIENT_ID,
-  environment.GOOGLE_CLIENT_SECRET,
-  environment.GOOGLE_CALENDAR_REDIRECT_URL,
+    environment.GOOGLE_CLIENT_ID,
+    environment.GOOGLE_CLIENT_SECRET,
+    environment.GOOGLE_CALENDAR_REDIRECT_URL
 );
 
 const OauthClient = client;
