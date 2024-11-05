ecb35c2afb44dbebf80f2e2bad58e1bdbff12f5a
joaorceschini <jrvceschini@gmail.com>
2024-11-05 13:06:40 -0300

    refac: starting fe imp today page

--- apps/frontend/src/components/atoms/Date.tsx
+++ apps/frontend/src/components/atoms/Date.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 
+import { addDays, startOfWeek, endOfWeek, format } from "date-fns"
 import { ListFilter } from "lucide-react"
 
 import { LeftChevron, RightChevron } from "@/src/lib/icons/Navigation"
@@ -10,7 +11,7 @@ const formatDate = (date: Date) => {
   const month = date.toLocaleDateString("en-US", { month: "short" })
   const day = date.getDate()
 
-  return `${weekday}, ${month} ${day}` // Desired format
+  return `${weekday}, ${month} ${day}`.toLowerCase()
 }
 
 interface DateCycleProps {
@@ -37,36 +38,50 @@ export const DateCycle: React.FC<DateCycleProps> = ({
     onDateChange(newDate)
   }
 
+  const goToToday = () => {
+    const today = new Date()
+    const localToday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    )
+    onDateChange(localToday)
+  }
+
   const goToNextDay = () => {
     const newDate = new Date(selectedDate)
     newDate.setDate(newDate.getDate() + 1)
     onDateChange(newDate)
   }
 
+  const formatedDateTitle = format(selectedDate, "eee").toLowerCase()
+
   const isToday = selectedDate.toDateString() === new Date().toDateString()
 
   return (
-    <div className="space-y-2">
-      <div className="flex items-center justify-between">
-        <div className="flex w-48 items-center gap-2 text-secondary-foreground">
-          <ListFilter size={16} />
-          <p className="text-sm">{formatDate(selectedDate)}</p>
+    <div className="flex w-full flex-col gap-4 pl-5 text-sm">
+      <div className="flex w-full items-center justify-between gap-5">
+        <div className="flex items-center gap-2 text-secondary-foreground">
+          <span className="text-sm">{formatDate(selectedDate)}</span>
         </div>
-        <div className="flex items-center gap-2">
-          <button onClick={goToPreviousDay} className="p-2">
-            <LeftChevron />
+        <div className="flex items-center gap-2 text-secondary-foreground">
+          <button onClick={goToPreviousDay} className="px-1">
+            <LeftChevron className="hover-text" />
           </button>
-          <button onClick={goToNextDay} className="p-2">
-            <RightChevron />
+          <button onClick={goToToday}>
+            <span className="hover-text">today</span>
+          </button>
+          <button onClick={goToNextDay} className="px-1">
+            <RightChevron className="hover-text" />
           </button>
         </div>
       </div>
-      {isToday && (
-        <div className="flex items-center gap-2 text-sm">
-          <h1 className="font-semibold text-foreground">Today</h1>
-          <p className="text-secondary-foreground">{totalItems}</p>
-        </div>
-      )}
+      <div className="flex items-center gap-2">
+        <h1 className="font-semibold text-foreground">
+          {isToday ? "today" : formatedDateTitle}
+        </h1>
+        <p className="text-secondary-foreground">{totalItems}</p>
+      </div>
     </div>
   )
 }
