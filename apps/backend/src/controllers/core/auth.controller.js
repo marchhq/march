281f1a34770a7aecbfe1084fd093022015e1c626
sajdkabir <sajda.kbir@gmail.com>
2024-11-06 10:16:19 +0530

    cleanup: remove login with email

--- apps/backend/src/controllers/core/auth.controller.js
+++ apps/backend/src/controllers/core/auth.controller.js
@@ -1,40 +1,8 @@
-import Joi from "joi";
-import { createEmailUser, validateEmailUser, validateGoogleUser, getUserByEmail, createGoogleUser, createGithubUser, validateGithubUser } from "../../services/core/user.service.js";
+import { validateEmailUser, validateGoogleUser, getUserByEmail, createGoogleUser, createGithubUser, validateGithubUser } from "../../services/core/user.service.js";
 import { generateJWTTokenPair } from "../../utils/jwt.service.js";
-import { RegisterPayload, LoginPayload } from "../../payloads/core/auth.payload.js";
+import { LoginPayload } from "../../payloads/core/auth.payload.js";
 import { BlackList } from "../../models/core/black-list.model.js";
 import { spaceQueue } from "../../loaders/bullmq.loader.js";
-
-const { ValidationError } = Joi;
-
-const registerEmailUserController = async (req, res, next) => {
-    try {
-        const { fullName, userName, email, password } = await RegisterPayload.validateAsync({ fullName: req.body.fullName, email: req.body.email, password: req.body.password });
-        const user = await createEmailUser({
-            fullName,
-            userName,
-            email,
-            password
-        })
-        if (!user) {
-            throw new Error("Failed to create user");
-        }
-        const { ok, isNewUser } = await generateJWTTokenPair(user);
-
-        res.status(200).json({
-            statusCode: 200,
-            response: {
-                ok,
-                isNewUser
-            }
-        })
-        // TODO: Send welcome email and verify email template to user
-    } catch (err) {
-        const error = new Error(err)
-        error.statusCode = err instanceof ValidationError ? 400 : (err.statusCode || 500)
-        next(error);
-    }
-}
 
 const emailLoginController = async (req, res, next) => {
     try {
@@ -154,7 +122,6 @@ const logOutController = async (req, res, next) => {
 }
 
 export {
-    registerEmailUserController,
     emailLoginController,
     authenticateWithGoogleController,
     authenticateWithGithubController,
