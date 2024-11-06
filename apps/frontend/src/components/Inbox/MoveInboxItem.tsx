909481766ee1b5582979679d3eff49f1f9a5be46
Madhav <madhavsingh.relish@gmail.com>
2024-11-06 11:27:26 +0530

    Added basen modal for moveinbox items

---
+++ apps/frontend/src/components/Inbox/MoveInboxItem.tsx
@@ -0,0 +1,15 @@
+'use clent'
+
+import { useModal } from '@/src/contexts/ModalProvider'
+import React from 'react'
+
+type Props = {}
+
+const MoveInboxItem = (props: Props) => {
+   
+  return (
+    <div>MoveInboxItem</div>
+  )
+}
+
+export default MoveInboxItem
