ab84150c831fc11ac35a9a5b865f0128f4ac969d
sajdkabir <sajda.kbir@gmail.com>
2024-11-07 19:58:10 +0530

    feat: verifyGithubSignature

--- apps/backend/src/controllers/integration/github.controller.js
+++ apps/backend/src/controllers/integration/github.controller.js
@@ -1,4 +1,6 @@
+import { environment } from "../../loaders/environment.loader.js";
 import { processWebhookEvent, exchangeCodeForAccessToken } from "../../services/integration/github.service.js";
+import * as crypto from "crypto";
 
 const handleGithubCallbackController = async (req, res, next) => {
     try {
@@ -19,10 +21,30 @@ const handleGithubCallbackController = async (req, res, next) => {
     }
 };
 
+const verifyGithubSignature = (req) => {
+    const signature = req.headers['x-hub-signature-256'];
+    const payload = JSON.stringify(req.body);
+    const GITHUB_WEBHOOK_SECRET = environment.GITHUB_WEBHOOK_SECRET
+
+    if (!signature || !payload || !GITHUB_WEBHOOK_SECRET) {
+        return false;
+    }
+
+    const hmac = crypto.createHmac('sha256', GITHUB_WEBHOOK_SECRET);
+    const digest = 'sha256=' + hmac.update(payload).digest('hex');
+
+    return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest));
+};
+
 const handleGithubWebhook = async (req, res, next) => {
     try {
+        if (!verifyGithubSignature(req)) {
+            return res.status(401).send({ message: 'Invalid signature' });
+        }
+
         const event = req.headers['x-github-event'];
         const payload = req.body;
+
         await processWebhookEvent(event, payload);
 
         res.status(200).send({ message: 'Webhook received and processed' });
