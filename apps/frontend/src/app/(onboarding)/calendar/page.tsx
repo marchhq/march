bd4c2534b9fad87dc35541287931b118a78e673c
deewakar <dakshftw01@gmail.com>
2024-11-11 12:52:54 +0530

    refac: calendar

--- apps/frontend/src/app/(onboarding)/calendar/page.tsx
+++ apps/frontend/src/app/(onboarding)/calendar/page.tsx
@@ -1,21 +1,24 @@
-import { Icon } from "@iconify-icon/react"
 import Link from "next/link"
 
-import CalendarAuth from "@/src/components/atoms/CalendarAuth"
-import CalendarList from "@/src/components/atoms/CalendarList"
+import CalendarLogin from "@/src/components/login/CalendarLogin"
 
 const CalendarConnect = () => {
   return (
     <main className="flex h-full flex-col items-center justify-between">
-      <div className="flex size-full flex-col items-center justify-center gap-12">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="font-medium">Connect Your Calendar</h1>
-        </div>
-        <div className="flex flex-col gap-1 text-base font-medium text-secondary-foreground">
-          <CalendarList />
+      <div className="flex size-3/4 flex-col items-center justify-center gap-12">
+        <div className="flex flex-col justify-center gap-4">
+          <div className="flex flex-col gap-2">
+            <h1 className="font-medium">
+              Connect Your{" "}
+              <span className="text-primary-foreground">calendar</span>
+            </h1>
+            <p className="max-w-[320px] pl-1 text-start text-xs font-semibold text-secondary-foreground">
+              sync your daily agenda with march and take notes
+            </p>
+          </div>
         </div>
         <div className="flex w-full flex-col items-center gap-2 text-base">
-          <CalendarAuth />
+          <CalendarLogin />
         </div>
       </div>
       <div className="w-full text-sm text-secondary-foreground">
