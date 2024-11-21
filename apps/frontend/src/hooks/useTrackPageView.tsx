fb24ce2b6d7ac6389080d95f8319aa5f60599151
Madhav <madhavsingh.relish@gmail.com>
2024-11-21 12:18:02 +0530

    Added userId for new users

--- apps/frontend/src/hooks/useTrackPageView.tsx
+++ apps/frontend/src/hooks/useTrackPageView.tsx
@@ -14,6 +14,7 @@ export function useTrackPageView(userId: string | null) {
       track({
         channel: "user-activity",
         event: `Page View ${pathname}`,
+        user_id: userId,
         description: `User visited ${pathname}`,
         tags: {
           page: pathname,
