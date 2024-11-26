7749864c296a56ddaaf71ba250784eec7b78989b
deewakar <dakshftw01@gmail.com>
2024-11-26 18:37:51 +0530

    fix: reading list

--- apps/frontend/src/components/Reading/ReadingListComponent.tsx
+++ apps/frontend/src/components/Reading/ReadingListComponent.tsx
@@ -1,14 +1,12 @@
 "use client"
 
-import React, { useEffect, useCallback, useState } from "react"
+import React, { useEffect, useCallback } from "react"
 
 import { ReadingExpandModal } from "./ReadingExpandModal"
 import AddItemForm from "@/src/components/Reading/AddItemForm"
 import ItemsList from "@/src/components/Reading/ItemsList"
 import { useAuth } from "@/src/contexts/AuthContext"
-import { useSpace } from "@/src/hooks/useSpace"
 import { ReadingItem } from "@/src/lib/@types/Items/Reading"
-import useBlockStore from "@/src/lib/store/block.store"
 import useReadingStore from "@/src/lib/store/reading.store"
 
 interface ReadingListComponentProps {
@@ -21,17 +19,24 @@ const ReadingListComponent: React.FC<ReadingListComponentProps> = ({
   blockId,
 }) => {
   const { session } = useAuth()
+  const {
+    fetchReadingList,
+    readingItems,
+    currentItem,
+    setCurrentItem,
+    isFetched,
+  } = useReadingStore()
 
-  const { fetchReadingList, readingItems, currentItem, setCurrentItem } =
-    useReadingStore()
-
+  // Only fetch if not already fetched
   useEffect(() => {
     const loadReadingListItems = async () => {
-      await fetchReadingList(session, spaceId, blockId)
+      if (!isFetched) {
+        await fetchReadingList(session, spaceId, blockId)
+      }
     }
 
     loadReadingListItems()
-  }, [session, blockId, spaceId, fetchReadingList])
+  }, [session, blockId, spaceId, fetchReadingList, isFetched])
 
   const handleExpand = useCallback(
     (item: ReadingItem) => {
@@ -42,26 +47,28 @@ const ReadingListComponent: React.FC<ReadingListComponentProps> = ({
     [currentItem, setCurrentItem]
   )
 
+  if (!isFetched) {
+    return <div>Loading...</div>
+  }
+
   return (
     <section className="size-full overflow-y-auto bg-background text-secondary-foreground">
       <div className="p-16 px-8 pb-8 sm:px-8 lg:px-14">
-        {blockId && (
-          <div className="flex w-3/4 flex-col gap-3 text-base">
-            <div className="sticky top-0 z-10 grid bg-background">
-              <AddItemForm blockId={blockId} spaceId={spaceId} />
-            </div>
-            <div className="flex-1">
-              <ItemsList
-                blockId={blockId}
-                spaceId={spaceId}
-                items={readingItems}
-                handleExpand={handleExpand}
-              />
-            </div>
+        <div className="flex w-3/4 flex-col gap-3 text-base">
+          <div className="sticky top-0 z-10 grid bg-background">
+            <AddItemForm blockId={blockId} spaceId={spaceId} />
           </div>
-        )}
+          <div className="flex-1">
+            <ItemsList
+              blockId={blockId}
+              spaceId={spaceId}
+              items={readingItems}
+              handleExpand={handleExpand}
+            />
+          </div>
+        </div>
       </div>
-      {blockId && <ReadingExpandModal blockId={blockId} spaceId={spaceId} />}
+      <ReadingExpandModal blockId={blockId} spaceId={spaceId} />
     </section>
   )
 }
