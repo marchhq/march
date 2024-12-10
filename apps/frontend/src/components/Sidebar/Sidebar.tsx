f2cf02c064e7594a3ce5de8256c05da5e98daa76
deewakar <dakshftw01@gmail.com>
2024-12-10 12:59:03 +0530

    feat: dock sidebar

--- apps/frontend/src/components/Sidebar/Sidebar.tsx
+++ apps/frontend/src/components/Sidebar/Sidebar.tsx
@@ -20,11 +20,19 @@ const SidebarNav: React.FC = () => {
   const { user } = useUserInfo()
   useTrackPageView(user?.userName || "")
   return (
-    <nav className="flex h-screen bg-background p-6 text-sm text-secondary-foreground ">
+    <nav className="flex h-screen bg-background p-4 text-sm text-secondary-foreground ">
       <div className="flex h-5/6 flex-col justify-center gap-4">
-        <SearchIcon size={18} className="cursor-pointer text-white" />
-        <BracketsIcon size={18} className="cursor-pointer text-white" />
-        <SidebarFeedback />
+        <div className="space-y-6 rounded-full border border-border p-3">
+          <SearchIcon
+            size={18}
+            className="cursor-pointer text-primary-foreground hover:text-white"
+          />
+          <BracketsIcon
+            size={18}
+            className="cursor-pointer text-primary-foreground hover:text-white"
+          />
+          <SidebarFeedback />
+        </div>
       </div>
     </nav>
   )
