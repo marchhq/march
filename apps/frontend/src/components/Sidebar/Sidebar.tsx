4ea57a9aff0187ffd0ef4d18bb66a3542527d4a7
deewakar <dakshftw01@gmail.com>
2024-12-10 12:31:26 +0530

    sidebar init

--- apps/frontend/src/components/Sidebar/Sidebar.tsx
+++ apps/frontend/src/components/Sidebar/Sidebar.tsx
@@ -2,70 +2,30 @@
 
 import React from "react"
 
+import { BracketsIcon, SearchIcon } from "lucide-react"
+
 import { SidebarFeedback } from "./SidebarFeedback"
-import { Switch } from "../atoms/Switch"
-import { SidebarFavorites } from "@/src/components/Sidebar/SidebarFavorites"
-import { SidebarMain } from "@/src/components/Sidebar/SidebarMain"
-import { SidebarProfile } from "@/src/components/Sidebar/SidebarProfile"
-import { SidebarSpaces } from "@/src/components/Sidebar/SidebarSpaces"
-import {
-  SidebarCollapseProvider,
-  useSidebarCollapse,
-} from "@/src/contexts/SidebarCollapseContext"
 import { useTrackPageView } from "@/src/hooks/useTrackPageView"
 import { useUserInfo } from "@/src/hooks/useUserInfo"
-import classNames from "@/src/utils/classNames"
 
 export const Sidebar: React.FC = () => {
   return (
-    <SidebarCollapseProvider>
-      <div className="flex flex-col border-r border-border">
-        <div className="pl-6 pt-1">
-          <SidebarCollapseButton />
-        </div>
-        <SidebarNav />
-      </div>
-    </SidebarCollapseProvider>
+    <div className="flex flex-col">
+      <SidebarNav />
+    </div>
   )
 }
 
 const SidebarNav: React.FC = () => {
-  const { isCollapsed } = useSidebarCollapse()
   const { user } = useUserInfo()
   useTrackPageView(user?.userName || "")
   return (
-    <nav
-      className={classNames(
-        "group relative flex h-screen bg-background text-sm p-6 text-secondary-foreground ",
-        isCollapsed ? "w-[250px]" : "w-[80px]"
-      )}
-    >
-      <div className="flex h-5/6 flex-col justify-between">
-        <div className="flex flex-col gap-4">
-          <SidebarMain />
-          <SidebarFavorites />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <SidebarProfile />
-          <SidebarFeedback />
-        </div>
+    <nav className="flex h-screen bg-background p-6 text-sm text-secondary-foreground ">
+      <div className="flex h-5/6 flex-col justify-center gap-4">
+        <SearchIcon size={18} className="cursor-pointer text-white" />
+        <BracketsIcon size={18} className="cursor-pointer text-white" />
+        <SidebarFeedback />
       </div>
-
-      {isCollapsed && (
-        <div className="mr-4 mt-8">
-          <SidebarSpaces />
-        </div>
-      )}
     </nav>
-  )
-}
-
-const SidebarCollapseButton: React.FC = () => {
-  const { isCollapsed, toggleCollapse } = useSidebarCollapse()
-  return (
-    <div className="-ml-2.5">
-      <Switch checked={isCollapsed} onCheckedChange={toggleCollapse} />
-    </div>
   )
 }
