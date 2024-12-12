0e4d88c727344dba3b876594a771601feac36650
sajdkabir <sajda.kbir@gmail.com>
2024-12-12 19:37:12 +0530

    remove the testing code

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   output: "standalone",
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
