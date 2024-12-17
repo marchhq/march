08ff9e7fe5e8ebba0dc7ecc080d22457c2ce6fd5
deewakar <dakshftw01@gmail.com>
2024-12-17 10:57:03 +0530

    feat: added sys pref & persist theme

--- apps/frontend/src/lib/store/theme.store.ts
+++ apps/frontend/src/lib/store/theme.store.ts
@@ -1,24 +1,35 @@
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
 
-export type Theme = "light" | "dark"
+type Theme = "light" | "dark"
 
 interface ThemeState {
   theme: Theme
   setTheme: (theme: Theme) => void
+  detectSystemTheme: () => void
 }
 
 export const useThemeStore = create<ThemeState>()(
   persist(
     (set) => ({
-      theme:
-        typeof window !== "undefined"
-          ? window.localStorage.getItem("theme-storage")
-            ? JSON.parse(window.localStorage.getItem("theme-storage")!).state
-                .theme
-            : "light"
-          : "light",
-      setTheme: (theme) => set({ theme }),
+      theme: "dark",
+      setTheme: (theme) => {
+        set({ theme })
+        const root = window.document.documentElement
+        root.setAttribute("data-theme", theme)
+        if (theme === "dark") {
+          root.classList.add("dark")
+        } else {
+          root.classList.remove("dark")
+        }
+      },
+      detectSystemTheme: () => {
+        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+          .matches
+          ? "dark"
+          : "light"
+        set({ theme: systemTheme })
+      },
     }),
     {
       name: "theme-storage",
