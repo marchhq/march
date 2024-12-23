1739947a3248d243fc44360b816470319b39cdd4
deewakar <dakshftw01@gmail.com>
2024-12-23 13:20:30 +0530

    ui: view wrapper and add bookmark

--- apps/frontend/src/app/(app)/objects/[type]/page.tsx
+++ apps/frontend/src/app/(app)/objects/[type]/page.tsx
@@ -1,3 +1,4 @@
+import { BookmarkView } from "@/src/components/bookmarks/BookmarkView"
 import { TodoView } from "@/src/components/todo/TodoView"
 
 interface Props {
@@ -12,6 +13,8 @@ export default async function ObjectPage({ params }: Props) {
   switch (type) {
     case "todo":
       return <TodoView />
+    case "bookmarks":
+      return <BookmarkView />
     default:
       return <div>{type} view</div>
   }
