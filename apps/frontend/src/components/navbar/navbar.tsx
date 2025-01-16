36f5e1505e641c3c4b8329e5080ee755052f0015
manashbarai <manashjbarai@gmail.com>
2025-01-16 22:46:53 +0530

    fix:navbar logged in then show navbar otherwise not showing and set user avatar and click out side of navbar dropown hide automatic

--- apps/frontend/src/components/navbar/navbar.tsx
+++ apps/frontend/src/components/navbar/navbar.tsx
@@ -1,22 +1,25 @@
-"use client"
 
 import Link from "next/link"
 import Image from "next/image"
-import { usePathname } from "next/navigation"
 
 import { NavDropdown } from "./nav-dropdown"
-import { useAuth } from "@/src/contexts/AuthContext"
-import useUserStore from "@/src/lib/store/user.store"
+// import { useAuth } from "@/src/contexts/AuthContext"
+// import useUserStore from "@/src/lib/store/user.store"
+import { User } from "lucide-react"
+import { getUserProfile } from "@/src/lib/server/fetcher/getMyProfile"
+import { ACCESS_TOKEN } from "@/src/lib/constants/cookie"
+import { cookies } from "next/headers"
+export const Navbar = async () => {
 
-export const Navbar = () => {
-  const pathname = usePathname()
-  const { session } = useAuth()
-  const { user } = useUserStore()
+  // const { session } = useAuth()
+  // const { user } = useUserStore()
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get(ACCESS_TOKEN)?.value as string;
+  const user = await getUserProfile(accessToken)
 
-  console.log('user', user)
 
   return (
-    <nav className="flex h-14 items-center">
+    <nav className={` ${user === null ? 'hidden' : 'flex'}  h-14 items-center`}>
       <div className="flex items-center justify-between w-full px-8">
         <div className="flex items-center">
           {/* Logo */}
@@ -35,7 +38,7 @@ export const Navbar = () => {
 
           {/* Navigation Dropdown */}
           <div className="ml-2">
-            <NavDropdown currentPath={pathname} />
+            <NavDropdown />
           </div>
         </div>
 
@@ -43,7 +46,15 @@ export const Navbar = () => {
         <div className="flex items-center pr-4">
           <Link href="/profile" className="flex items-center">
             <div className="w-8 h-8 rounded-full bg-gray-100 flex items-center justify-center text-sm font-medium text-gray-700">
-              {user?.userName?.[0]?.toUpperCase() || 'U'}
+              {/* {user?.userName?.[0]?.toUpperCase() || 'U'} */}
+              {user ? <Image
+                src={user.avatar}
+                width={24}
+                height={24}
+                className="rounded-full"
+                alt={user?.fullName || "User avatar"}
+              /> : <User />}
+
             </div>
           </Link>
         </div>
