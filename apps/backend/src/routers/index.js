81710db779b4b6f55c23deab06f5bf64c219daaf
sajdkabir <sajda.kbir@gmail.com>
2025-02-06 13:16:16 +0530

    fix: greeting api

--- apps/backend/src/routers/index.js
+++ apps/backend/src/routers/index.js
@@ -26,10 +26,10 @@ const initRoutes = (app) => {
     app.use('/github', JWTMiddleware, GithubRoute);
     app.use('/notion', JWTMiddleware, NotionRoute);
     app.use('/ai', JWTMiddleware, AiRoute);
-    app.use('/', JWTMiddleware, TypeRoute);
+    // app.use('/', JWTMiddleware, TypeRoute);
     app.get("/", async (req, res) => {
         res.json({
-            "message": "Welcome to March Developers Portal"
+            "message": "Welcome to emptyarray Developers Portal"
         })
     })
 
