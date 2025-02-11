a36315d4e0665ee943114fca445bc6151462941d
deewakar <dakshftw01@gmail.com>
2025-02-11 17:32:10 +0530

    fix: color

--- apps/frontend/src/components/ui/resizable.tsx
+++ apps/frontend/src/components/ui/resizable.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { GripVertical } from "lucide-react"
 import * as ResizablePrimitive from "react-resizable-panels"
 
-import { cn } from "@/lib/utils"
+import { GripVertical } from "lucide-react"
+
+import { cn } from "@/src/utils/utils"
 
 const ResizablePanelGroup = ({
   className,
@@ -36,7 +37,7 @@ const ResizableHandle = ({
   >
     {withHandle && (
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border border-neutral-200 bg-neutral-200 dark:border-neutral-800 dark:bg-neutral-800">
-        <GripVertical className="h-2.5 w-2.5" />
+        <GripVertical className="size-2.5" />
       </div>
     )}
   </ResizablePrimitive.PanelResizeHandle>
