3a610d7636c39d8dc673a1ce01555936594045c9
deewakar <dakshftw01@gmail.com>
2025-02-20 09:56:43 +0530

    chore

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,3 @@
-import ListBlock from "@/components/blocks/list/list";
-
 export default function Home() {
-  return (
-    <main className="min-h-screen pt-4">
-      <ListBlock header="Inbox" />
-    </main>
-  );
+  return <div>landing page</div>;
 }
