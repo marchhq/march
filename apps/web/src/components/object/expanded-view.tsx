4fef91b816bac23653565ebe2afa14b596deafd6
deewakar-k <dakshftw01@gmail.com>
2025-02-28 13:47:22 +0530

    feat: responsive obj sheet

--- apps/web/src/components/object/expanded-view.tsx
+++ apps/web/src/components/object/expanded-view.tsx
@@ -8,7 +8,10 @@ import {
 
 export default function ExpandedView({ item }: { item: Objects }) {
   return (
-    <SheetContent side={"right"} className="">
+    <SheetContent
+      side="right"
+      className="sm:max-w-md md:max-w-xl lg:max-w-2xl xl:max-w-3xl w-full"
+    >
       <SheetHeader>
         <SheetTitle>{item.title}</SheetTitle>
         <SheetDescription>{item.description}</SheetDescription>
