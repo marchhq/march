cdca3191deb5cd1bd1e6f8f23a62514967c0fa76
deewakar-k <dakshftw01@gmail.com>
2025-02-28 14:43:31 +0530

    fix: build error

--- apps/web/src/components/blocks/calendar/calendar-header.tsx
+++
@@ -1,9 +0,0 @@
-import { useCalendar } from "@/contexts/calendar-context";
-
-export default function CalendarHeader() {
-  return (
-    <div className="pt-4 pb-3 px-4">
-      {/* Navigation buttons removed */}
-    </div>
-  );
-}
