58204fd169ed0eab504dbc52705559931d86eb31
deewakar-k <dakshftw01@gmail.com>
2025-03-04 14:58:56 +0530

    feat: added bubble menu

--- apps/web/src/components/editor/editor.tsx
+++ apps/web/src/components/editor/editor.tsx
@@ -3,6 +3,13 @@
 import { EditorContent, EditorRoot, type JSONContent } from "novel";
 import { useRef, useState } from "react";
 import { defaultExtensions } from "./extentions";
+import "./prosemirror.css";
+import EditorMenu from "./editor-menu";
+import { NodeSelector } from "./selectors/node-selector";
+import { Separator } from "../ui/separator";
+import { LinkSelector } from "./selectors/link-selector";
+import { TextButtons } from "./selectors/text-buttons";
+import { ColorSelector } from "./selectors/color-selector";
 
 const extensions = [...defaultExtensions];
 
@@ -28,8 +35,18 @@ const Editor = ({
   placeholder = "write something...",
 }: EditorProps) => {
   const contentRef = useRef<string>("");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isNodeSelectorOpen, setIsNodeSelectorOpen] = useState(false);
+  const [isLinkSelectorOpen, setIsLinkSelectorOpen] = useState(false);
+  const [isColorSelectorOpen, setIsColorSelectorOpen] = useState(false);
 
   if (!initialValue) return null;
+
+  const handleUpdate = () => {
+    const content = contentRef.current;
+    onChange(content);
+  };
+
   return (
     <EditorRoot>
       <EditorContent
@@ -44,11 +61,22 @@ const Editor = ({
             "data-placeholder": placeholder,
           },
         }}
-        onUpdate={({ editor }) => {
-          contentRef.current = editor.getHTML();
-          onChange?.(contentRef.current);
-        }}
-      />
+        onUpdate={handleUpdate}
+      >
+        <EditorMenu open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+          <NodeSelector
+            open={isNodeSelectorOpen}
+            onOpenChange={setIsNodeSelectorOpen}
+          />
+          <Separator orientation="vertical" />
+          <LinkSelector
+            open={isLinkSelectorOpen}
+            onOpenChange={setIsLinkSelectorOpen}
+          />
+          <Separator orientation="vertical" />
+          <TextButtons />
+        </EditorMenu>
+      </EditorContent>
     </EditorRoot>
   );
 };
---
+++ apps/web/src/components/editor/slash-command.ts
@@ -0,0 +1,94 @@
+import {
+  CheckSquare,
+  Code,
+  Heading1,
+  Heading2,
+  Heading3,
+  ImageIcon,
+  List,
+  ListOrdered,
+  Text,
+  TextQuote,
+  Twitter,
+  Youtube
+} from 'lucide-react'
+import { createSuggestionItems } from 'novel'
+import { Command, renderItems } from 'novel'
+
+export const suggestionItems = createSuggestionItems([
+  {
+    title: 'Text',
+    description: 'Just start typing with plain text.',
+    searchTerms: ['p', 'paragraph'],
+    icon: <Text size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .toggleNode('paragraph', 'paragraph')
+        .run()
+    }
+  },
+  
+  {
+    title: 'Heading 1',
+    description: 'Big section heading.',
+    searchTerms: ['title', 'big', 'large'],
+    icon: <Heading1 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 1 })
+        .run()
+    }
+  },
+  {
+    title: 'Heading 2',
+    description: 'Medium section heading.',
+    searchTerms: ['subtitle', 'medium'],
+    icon: <Heading2 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 2 })
+        .run()
+    }
+  },
+  {
+    title: 'Heading 3',
+    description: 'Small section heading.',
+    searchTerms: ['subtitle', 'small'],
+    icon: <Heading3 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 3 })
+        .run()
+    }
+  },
+  {
+    title: 'Bullet List',
+    description: 'Create a simple bullet list.',
+    searchTerms: ['unordered', 'point'],
+    icon: <List size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleBulletList().run()
+    }
+  },
+  {
+    title: 'Numbered List',
+    description: 'Create a list with numbering.',
+    searchTerms: ['ordered'],
+    icon: <ListOrdered size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleOrderedList().run()
+    }
+  }
+])
