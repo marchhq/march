eaab8bada26d9d34915b72c3a7da61ea293f04b1
deewakar-k <dakshftw01@gmail.com>
2025-03-05 10:58:49 +0530

    feat: hero

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,20 @@
+import Hero from "@/components/home/hero";
+import { Section } from "@/components/home/section";
+
 export default function Home() {
-  return <div>landing page</div>;
+  return (
+    <div className="flex w-full flex-col items-center pt-24 md:pt-0">
+      <PageContent />
+    </div>
+  );
 }
+
+export const PageContent = () => {
+  return (
+    <>
+      <Section className="flex flex-col gap-y-24">
+        <Hero />
+      </Section>
+    </>
+  );
+};
---
+++ apps/web/src/components/home/section.tsx
@@ -0,0 +1,34 @@
+import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+
+export type SectionProps = PropsWithChildren<{
+  id?: string;
+  className?: string;
+  wrapperClassName?: string;
+}>;
+
+export const Section = ({
+  id,
+  className,
+  wrapperClassName,
+  children,
+}: SectionProps) => {
+  return (
+    <div
+      id={id}
+      className={twMerge(
+        "relative flex w-full flex-col items-center justify-stretch",
+        wrapperClassName
+      )}
+    >
+      <div
+        className={twMerge(
+          "flex w-full max-w-[100vw] flex-col px-4 py-16 md:max-w-7xl md:px-12 md:py-16",
+          className
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
