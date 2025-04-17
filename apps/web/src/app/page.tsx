8fc7979f23fccbad6da3a85f839bfd77da1bc2a4
sajda <sajda.kbir@gmail.com>
2025-04-17 22:35:37 +0530

    Revert "landing page"

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,32 +1,3 @@
-// import Hero from "@/components/home/hero";
-// import { Section } from "@/components/home/section";
-
-// export const PageContent = () => {
-//   return (
-//     <>
-//       <Section className="flex flex-col gap-y-24">
-//         <Hero />
-//       </Section>
-//     </>
-//   );
-// };
-
-// export default function Home() {
-//   return (
-//     <div className="flex w-full flex-col items-center pt-24 md:pt-0">
-//       <PageContent />
-//     </div>
-//   );
-// }
-
-
-
-import PageContent from "@/components/home/page-content";
-
 export default function Home() {
-  return (
-    <div className="flex w-full flex-col items-center pt-24 md:pt-0">
-      <PageContent />
-    </div>
-  );
+  return <div>landing page</div>;
 }
